55264f1d994de00c14c1ec0ab45f4c768d337087
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-09-17 14:24:30 +0200

    playground | more helpers from HIGHaims. REM: put playground on PYTHONPATH

---
+++ playground/helpers/linalg.py
@@ -0,0 +1,10 @@
+import numpy as np
+from libflo.konstanten.numerics import eps
+
+def clean_matrix(matrix):
+    """ clean from small values"""
+    matrix = np.array(matrix)
+    for ij in np.ndindex(matrix.shape):
+        if abs(matrix[ij]) < eps:
+            matrix[ij] = 0
+    return matrix
