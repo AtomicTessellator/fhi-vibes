42864517f9fbae9ebe34ea7e7eefd92977ac506b
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2018-09-26 16:38:39 +0200

    added scalar product ability to fingerprints

--- playground/MatRefDatabase/src/MaterialsFingerprints.py
+++ playground/MatRefDatabase/src/MaterialsFingerprints.py
@@ -11,16 +11,22 @@ class MaterialsFingerprint(object):
     isElec = False
     isDStream = False
 
-    def __init__(self, isElec, isB, nbins=0, fp={},kpoints={}, spectraFiles=[], spectraYAML="", minE=1e20, maxE=-1e20):
+    def __init__(self, isElec, isB, nbins=-1, dE=-1.0, fp={},kpoints={}, spectraFiles=[], spectraYAML="", minE=None, maxE=None):
         self.kpoints = kpoints
         self.spectraFiles = spectraFiles
         self.spectraYAML = spectraYAML
         self.isB = isB
         self.isElec = isElec
-        self.minE = minE
-        self.maxE = maxE
-        if( nbins == 0 ):
+        self.minE = minE if(minE !=None) else  1e20 if(isB) else np.min( np.genfromtxt(self.spectraFiles[0])[:,0] )
+        self.maxE = maxE if(maxE !=None) else -1e20 if(isB) else np.max( np.genfromtxt(self.spectraFiles[0])[:,0] )
+        self.nbins = nbins
+        self.dE    = dE
+        if( nbins == -1 and dE == -1.0 ):
             self.nbins = 32 if isB else 256
+            self.fingerprint = ( self.constructBEFingerprint() if isElec else self.constructBPFingerprint() ) if isB else self.constructDFingerprint()
+        elif nbins != -1:
+            self.fingerprint = ( self.constructBEFingerprint() if isElec else self.constructBPFingerprint() ) if isB else self.constructDFingerprint()
+        elif dE != -1.0:
             self.fingerprint = ( self.constructBEFingerprint() if isElec else self.constructBPFingerprint() ) if isB else self.constructDFingerprint()
         else:
             self.fingerprint = fp
@@ -34,15 +40,23 @@ class MaterialsFingerprint(object):
                 for ff in self.fingerprint[pt]:
                     frmt += ";%f;%f" % (ff[0], ff[1])
             return frmt
+        return ""
+
+    def getEner(self):
+        if(self.nbins != -1):
+            return np.linspace( self.minE, self.maxE, self.nbins+1 )
+        else:
+            return np.arange( self.minE, self.maxE+self.dE/10.0, self.dE )
 
     def constructDFingerprint(self):
-        fingerprint = { "DOS" : np.zeros(shape=(self.nbins, 2)) }
         dos = np.genfromtxt(self.spectraFiles[0])
-        ener = np.linspace( np.min( dos[:,0] ), np.max( dos[:,0] ), self.nbins+1 )
+        ener = self.getEner( )
+        fingerprint = { "DOS" : np.zeros(shape=(len(ener)-1, 2)) }
         dEner = (ener[1]-ener[0])
         fingerprint["DOS"][:,0] = ener[:-1] + dEner/2.0
         for ff in dos:
-            fingerprint["DOS"][np.where(fingerprint["DOS"][:,0]-dEner/(2.0-1e-10) <= ff[0])[0][-1], 1] += ff[1]
+            if(ff[0] >= self.minE and ff[0] <= self.maxE):
+                fingerprint["DOS"][np.where(fingerprint["DOS"][:,0]-dEner/(2.0-1e-10) <= ff[0])[0][-1], 1] += ff[1]
         return fingerprint
 
     def constructBEFingerprint(self):
@@ -103,7 +117,7 @@ class MaterialsFingerprint(object):
 
     def makeFingerPrintFromBand(self, bands):
         fingerprint = {}
-        ener = np.linspace(self.minE, self.maxE, self.nbins+1)
+        ener = self.getEner()
         for pt in bands:
             fingerprint[pt] = np.zeros(shape=(self.nbins,2))
             dEner = ener[1]-ener[0]
@@ -112,3 +126,21 @@ class MaterialsFingerprint(object):
                 if( self.minE <= ff and ff <= self.maxE ):
                     fingerprint[pt][np.where(fingerprint[pt][:,0]-dEner/(2.0-1e-10) <= ff)[0][-1], 1] += 1.0
         return fingerprint
+
+    def scalarProduct(self, otherFP):
+        if(len(self.fingerprint) != len(otherFP.fingerprint) ):
+            print("The two finger prints do not represent the same q points, the scalar product is zero")
+            return 0.0
+        scalar = 0.0
+        for key in self.fingerprint:
+            if key not in otherFP.fingerprint:
+                print("The two finger prints do not represent the same q points, the scalar product is zero")
+                return 0.0
+            elif(self.isB and np.sum(self.fingerprint[key][:,1]) != np.sum(otherFP.fingerprint[key][:,1]) ):
+                print("The two fingerprints should have the same number bands, the scalar product is 0")
+                return 0.0
+            elif( np.any(self.fingerprint[key][:,0] != otherFP.fingerprint[key][:,0] ) ):
+                print("The energy bins need to be exactly the same, the scalar product is 0.0")
+                return 0.0
+            scalar += np.dot(self.fingerprint[key][:,1], otherFP.fingerprint[key][:,1])/np.linalg.norm(self.fingerprint[key][:,1])**2
+        return scalar/len(self.fingerprint)
