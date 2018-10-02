0fb47852e7dc2b07e71a429734e716f470b28b71
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-10-02 15:24:32 +0200

    setup.py | order in sources matters, otherwise compile error

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ import setuptools
 from numpy.distutils.core import setup, Extension
 
 ext = Extension(name = 'hilde.helpers.supercell.supercell',
-                sources=['hilde/helpers/supercell/supercell.f90',
-                         'hilde/helpers/supercell/linalg.f90'],
+                sources=['hilde/helpers/supercell/linalg.f90',
+                         'hilde/helpers/supercell/supercell.f90'],
                 extra_compile_args = ['-O3'],
-)
+                )
 
 setup(name='HiLDe',
       version='0.1',
       description='Haber Institute Lattice Dynamics Package',
-      author='Thomas Purcell, Florian Knoop',
+      author='Florian Knoop, Thomas Purcell',
       author_email='knoop@fhi-berlin.mpg.de',
       license='MIT License',
       # install_requires=['numpy', 'phonopy', 'spglib'],
       packages=setuptools.find_packages(),
       ext_modules=[ext],
-      scripts=['hilde/scripts/geometry_info']
+      scripts=['hilde/scripts/geometry_info'],
+      zip_safe=True
       )
