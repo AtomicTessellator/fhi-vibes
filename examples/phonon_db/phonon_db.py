f577d4686d43aebbc908c7e06074aa4d1a2040f7
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-10-08 18:43:13 +0200

    phonon_db | some cosmetics, adjusted to hilde.conf location

--- examples/phonon_db/phonon_db.py
+++ examples/phonon_db/phonon_db.py
@@ -15,8 +15,11 @@ from hilde.structure import pAtoms
 from hilde.tasks.calculate import compute_forces
 
 # Get the settings for the calculation and set up the cell
-st = Settings('hilde.conf')
-database_dir = str(Path(st.database.location) / st.database.name) if not st.database.name.startswith('postgres') else st.database.name
+st = Settings('../../hilde.conf')
+if st.database.name.startswith('postgres'):
+    database_dir = st.database.name
+else:
+    database_dir = str(Path(st.database.location) / st.database.name)
 print(f'database: {database_dir}')
 
 aims_tmp_dir = Path(st.database.location) / 'aims_tmp'
@@ -45,7 +48,11 @@ force_sets = []
 found = False
 
 try:
-    rows = list( db.select( selection=[("supercell_matrix","=",smatrix), ("atoms_hash","=",atoms_hash), ("calc_hash","=",calc_hash), ("is_results","=",True)]) )
+    rows = list(db.select(
+        selection=[("supercell_matrix", "=", smatrix),
+                   ("atoms_hash", "=", atoms_hash),
+                   ("calc_hash", "=", calc_hash),
+                   ("is_results", "=", True)]))
     if not rows:
         raise KeyError('selection not found')
     else:
@@ -59,8 +66,8 @@ except KeyError:
         exit()
     phonon, sc, disp_scs = ph.preprocess(si, smatrix)
     vol = si.get_volume()
-    workdir = Path(str(aims_tmp_dir) + '/Si_{}{}{}_{}{}{}_{}{}{}_{:.3f}'.format(*smatrix.flatten(), vol))
-    workdir.mkdir(exist_ok=True)
+    workdir = Path(aims_tmp_dir / '{}_{}{}{}_{}{}{}_{}{}{}_{:.3f}'.format(
+        si.sysname, *smatrix.flatten(), vol))
     # Compute forces and phonon properties
     phonon.set_forces(compute_forces(disp_scs, si.calc, workdir))
     phonon.produce_force_constants()
@@ -79,22 +86,43 @@ except KeyError:
     phonon.set_total_DOS(freq_pitch=.1, tetrahedron_method=True )
     phonon.set_thermal_properties(t_step= 25, t_max = 1000, t_min = 0)
     # Write results to the database
-    db.write(phonon, atoms_hash=atoms_hash, calc_hash=calc_hash, is_results=(phonon.get_force_constants() is not None))
+    db.write(phonon,
+             atoms_hash=atoms_hash,
+             calc_hash=calc_hash,
+             is_results=(phonon.get_force_constants() is not None))
 
 # Example database operations
-row =list(db.select( selection=[("supercell_matrix","=",smatrix), ("atoms_hash","=",atoms_hash), ("calc_hash","=",calc_hash), ("is_results","=",True)], columns=["id", "qmesh", "thermal_prop_T", "thermal_prop_S", "thermal_prop_A","thermal_prop_Cv" ]))[0]
-print(f"The thermal properties for this set of calculations (k point mesh {row.qmesh}) are:")
-thermalProps = np.array([row.thermal_prop_T, row.thermal_prop_A, row.thermal_prop_S, row.thermal_prop_Cv]).transpose()
+row = list(db.select(
+    selection=[("supercell_matrix", "=", smatrix),
+               ("atoms_hash", "=", atoms_hash),
+               ("calc_hash", "=", calc_hash),
+               ("is_results", "=", True)],
+    columns=["id", "qmesh", "thermal_prop_T", "thermal_prop_S",
+             "thermal_prop_A","thermal_prop_Cv" ]))[0]
+print(f"The thermal properties for this set of calculations " +
+      f"(k point mesh {row.qmesh}) are:")
+thermalProps = np.array([row.thermal_prop_T, row.thermal_prop_A,
+                         row.thermal_prop_S, row.thermal_prop_Cv]).transpose()
 print(thermalProps)
 print(f"Recalculating the thermal properties with a mesh of [90, 90, 90]")
-phonon = db.get_phonon(selection=[("supercell_matrix","=",smatrix), ("atoms_hash","=",atoms_hash), ("calc_hash","=",calc_hash), ("is_results","=",True)])
-phonon.set_mesh([90,90,90])
+phonon = db.get_phonon(
+    selection=[("supercell_matrix", "=", smatrix),
+               ("atoms_hash", "=", atoms_hash),
+               ("calc_hash", "=", calc_hash),
+               ("is_results", "=", True)])
+phonon.set_mesh([90, 90, 90])
 phonon.set_thermal_properties(temperatures=row.thermal_prop_T)
-print(np.array( phonon.get_thermal_properties() ).transpose())
+print(np.array(phonon.get_thermal_properties()).transpose())
 try:
     print("The super cell matrix for this calculation was:")
     print(row.supercell_matrix)
 except:
-    print(f"supercell_matrix was not taken in the initial selection, resetting rows")
-    row =list(db.select( selection=[("supercell_matrix","=",smatrix), ("atoms_hash","=",atoms_hash), ("calc_hash","=",calc_hash), ("is_results","=",True)], columns=["supercell_matrix" ]))[0]
+    print(f"supercell_matrix was not taken in the initial selection, " +
+          f"resetting rows")
+    row =list(db.select(
+    selection=[("supercell_matrix", "=", smatrix),
+               ("atoms_hash", "=", atoms_hash),
+               ("calc_hash", "=", calc_hash),
+               ("is_results", "=", True)],
+               columns=["supercell_matrix"]))[0]
     print(row.supercell_matrix)
