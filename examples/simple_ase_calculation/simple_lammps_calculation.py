19ce36d8b95b80da72c6f42f68cbb04842f44dc9
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-10-12 11:01:22 +0200

    minor change to simple_lammps_calculation (include GaN)

--- examples/simple_ase_calculation/simple_lammps_calculation.py
+++ examples/simple_ase_calculation/simple_lammps_calculation.py
@@ -5,22 +5,46 @@ from hilde.tasks.calculate import calculate
 from ase.calculators.socketio import SocketIOCalculator, PySocketIO
 import os
 
-atoms = read('si.in', 0, 'aims')
 
-# LAMMPS context information
+# Lammps generic
 lmp_path = os.getenv("LAMMPS_PATH")
-potential = os.path.join(lmp_path, "potentials", "Si.tersoff")
-files = [potential]
-parameters = {"mass": ["* 1.0"],
-              "pair_style": "tersoff",
-              "pair_coeff": ['* * ' + potential + ' Si']}
 
-lammps = LAMMPS(parameters=parameters,
-                files=files)
+# Silicon
+# this is what hilde.templates.lammps.setup_si does
+def setup_si():
+    atoms = read('si.in', 0, 'aims')
+    potential = os.path.join(lmp_path, "potentials", "Si.tersoff")
+    files = [potential]
+    parameters = {"mass": ["* 1.0"],
+                "pair_style": "tersoff",
+                "pair_coeff": ['* * ' +  potential + ' Si']}
 
-# option 1:
-atoms.calc = lammps
-# REM: using .calculate(atoms) is mandatory for lammmps so far
-atoms.calc.calculate(atoms)
-print(atoms.get_total_energy())
-print(atoms.get_forces())
+    return atoms, parameters, files
+
+# GaN
+# this is what hilde.templates.lammps.setup_gan does
+def setup_gan():
+    atoms = read('gan.in', 0, 'aims')
+    potential = os.path.join(lmp_path, "potentials", "GaN.tersoff")
+    files = [potential]
+    parameters = {"mass": ["* 1.0"],
+                "pair_style": "tersoff",
+                "pair_coeff": ['* * ' +  potential + ' Ga N']}
+
+def main():
+    atoms, parameters, files = setup_si()
+
+    lammps = LAMMPS(parameters=parameters,
+                    files=files,
+                    tmp_dir='./lammps')
+
+
+    # option 1:
+    atoms.calc = lammps
+    # REM: using .calculate(atoms) is mandatory for lammmps so far
+    atoms.calc.calculate(atoms)
+    print(atoms.get_total_energy())
+    print(atoms.get_forces())
+
+
+main()
