85b0ba47eee6ae5e11e769f3cdb2a9f734c57909
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-10-15 17:50:48 +0200

    Examples | split of relaxation from phonons_socketio.py

--- examples/phonons/phonons_socketio.py
+++ examples/phonons/phonons_socketio.py
@@ -3,58 +3,33 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from time import time
 
-from hilde.parsers import read_structure, read_output
-from hilde.helpers.supercell import make_cubic_supercell
-from hilde.helpers import cwd, d2k, get_cubicness, clean_matrix
-from hilde.phonopy import phono as ph
-from hilde.tasks.calculate import calculate, calculate_multiple_socketio
 from ase.calculators.aims import Aims
 from ase.calculators.socketio import SocketIOCalculator
-from ase.dft.kpoints import get_cellinfo, special_paths, bandpath
+from hilde.parsers import read_structure
+from hilde.helpers.supercell import make_cubic_supercell
+from hilde.helpers import cwd, d2k, get_cubicness
+from hilde.phonopy import phono as ph
+from hilde.tasks.calculate import calculate, calculate_multiple_socketio
 from hilde.settings import Settings
+from hilde.templates.aims import setup_aims
+import hilde.helpers.brillouinzone as bz
 
 atoms = read_structure('../gan.in')
 vol = atoms.get_volume()
 
 base_folder = Path(f'{atoms.sysname}').absolute()
 
-# aims
-settings = Settings('../../hilde.conf')
-
-command = settings.machine.aims_command
-species_dir = str(Path(settings.machine.basissetloc) / 'light')
 port = 31415
-
 k_grid = d2k(atoms, 1)
+
 print(f'k_grid primitive cell: {k_grid}')
 
-aims_settings = {
-    'command': command,
-    'species_dir': species_dir,
-    'output_level': 'MD_light',
-    'relativistic': 'atomic_zora scalar',
-    'xc': 'pw-lda',
-    'k_grid': k_grid,
-    'sc_accuracy_rho': 1e-6,
-    'compute_forces': True
-}
-
-relax_settings = {
-    'relax_geometry': 'lattice_trm 1e-3',
-    'relax_unit_cell': 'full',
-    'sc_accuracy_forces': 1e-4,
-    'use_symmetric_forces': True
-}
-
 relax_folder = base_folder / f'relax_{vol:.3f}'
-
-aims = Aims(**{**aims_settings, **relax_settings})
 
 try:
     ratoms = read_structure(relax_folder / 'geometry.in.next_step')
 except FileNotFoundError:
-    calculate(atoms, aims, relax_folder)
-    ratoms = read_structure(relax_folder / 'geometry.in.next_step')
+    ratoms = atoms
 
 n_target = 10
 sc, smatrix = make_cubic_supercell(ratoms, target_size=n_target)
@@ -79,10 +54,12 @@ print(f'{len(scs)} supercells created.')
 
 socketio_settings = {
     'use_pimd_wrapper': ('localhost', port),
+    'compute_forces': True,
+    'sc_accuracy_rho': 1e-6,
     'k_grid': k_grid
 }
 
-aims = Aims(**{**aims_settings, **socketio_settings})
+aims = setup_aims(custom_settings=socketio_settings)
 
 phonopy_log = workdir/'phonopy.log'
 trajectory = workdir / 'phonopy.traj'
@@ -100,7 +77,14 @@ timing_socket_io = time() - stime
 print(f'.. done in {timing_socket_io:.2f}s')
 
 force_sets = [atoms.get_forces() for atoms in cells_computed]
+phonon.produce_force_constants(force_sets)
 
-dos = ph.get_dos(phonon, force_sets=force_sets)
+dos = ph.get_dos(phonon)
 plt.plot(dos[0], dos[1])
 plt.savefig(workdir / 'dos.pdf')
+
+#bands
+bands, labels = bz.get_bands_and_labels(phonon.primitive)
+phonon.set_band_structure(bands)
+phonon.plot_band_structure(labels=labels)
+plt.savefig(workdir / 'bandstructure.pdf')
