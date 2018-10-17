d3eba0d2317d9fd18ca7515f390b3febcad7d6fe
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-10-17 12:08:52 +0200

    phono(3)py | move to_pAtoms to phonopy, some polish

--- hilde/phono3py/phono3.py
+++ hilde/phono3py/phono3.py
@@ -6,44 +6,7 @@ from collections import namedtuple
 import numpy as np
 from phono3py.phonon3 import Phono3py
 from hilde import konstanten as const
-from hilde.structure import pAtoms
-
-
-def to_pAtoms(phonopy_atoms, smatrix, symprec=None):
-    """ Convert one or several PhonopyAtoms to pAtoms
-
-    Args:
-        phonopy_atoms (PhonopyAtoms/list): one or several PhonopyAtoms
-        smatrix (ndarray): Supercell matrix
-        symprec (float): symmetry precision
-
-    Returns:
-        pAtoms/list: one or several pAtoms
-
-    """
-
-    if isinstance(phonopy_atoms, list):
-        latoms = phonopy_atoms
-    else:
-        latoms = [phonopy_atoms]
-
-    out_atoms = []
-    for atoms in latoms:
-        # Check if the atoms does exist (important when working with cutoffs)
-        if atoms is None:
-            out_atoms.append(None)
-            continue
-
-        tags = ['supercell', ('smatrix', list(smatrix.T.flatten()))]
-        out_atoms.append(
-            pAtoms(phonopy_atoms=atoms,
-                   symprec=symprec,
-                   tags=tags)
-            )
-
-    if isinstance(phonopy_atoms, list):
-        return out_atoms
-    return out_atoms
+from hilde.phonopy import to_pAtoms
 
 
 def prepare_phono3py(atoms,
