71ee1302f47fc7541c38abca4fcd806efa15ac37
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-04 00:47:15 +0100

    md examples | use new aims template using new hilde.cfg

--- examples/md/md_with_socketio.py
+++ examples/md/md_with_socketio.py
@@ -1,56 +1,35 @@
-""" run an MD with socketio """
+""" run an MD for 2 steps with socketio """
 
-import sys
-from pathlib import Path
 import numpy as np
 from ase.io import read
-from ase.calculators.aims import Aims
 from ase.calculators.socketio import SocketIOCalculator
 from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import PhononHarmonics
 from ase import units
-from hilde.settings import Settings
-from hilde.helpers.paths import cwd
-
-
-# Read settings
-port = 27182
-settings = Settings("../../hilde.cfg")
-species_dir = str(Path(settings.machine.basissetloc) / "light")
-command = settings.machine.aims_command
-tmp_dir = Path("./tmp")
-tmp_dir.mkdir(parents=True, exist_ok=True)
+from hilde.templates.aims import setup_aims
 
 # Read input files
 atoms = read("Si.in.supercell", "0", "aims")
 force_constants = np.loadtxt("force_constants_Si.dat")
 
-# Some parameters
-temp = 100 * units.kB
-
-# Logging
-log_settings = {"trajectory": str(tmp_dir / "md.aims.traj"), "logfile": "md.aims.log"}
-
-# DFT
-aims_settings = {
-    "command": command,
+# use template aims calculator (taking settings from hilde.cfg) and custom settings
+port = 1234
+custom_settings = {
     "use_pimd_wrapper": ("localhost", port),
-    "species_dir": species_dir,
-    "output_level": "MD_light",
-    "xc": "pw-lda",
-    "k_grid": [2, 2, 2],
     "sc_accuracy_rho": 1e-4,
     "compute_forces": True,
 }
 
-calc = Aims(**aims_settings)
+calc = setup_aims(custom_settings, workdir="aims_tmp")
 
-# initialize positions + velocities
-PhononHarmonics(atoms, force_constants, temp=temp, quantum=False)
+# initialize positions + velocities at 100K
+PhononHarmonics(atoms, force_constants, temp=100 * units.kB, quantum=False)
 
-md = VelocityVerlet(atoms, timestep=4 * units.fs, **log_settings)
+md = VelocityVerlet(
+    atoms, timestep=4 * units.fs, trajectory="md.aims.traj", logfile="md.aims.log"
+)
 
 # run
-with SocketIOCalculator(calc, log=sys.stdout, port=port) as calc, cwd(tmp_dir):
+with SocketIOCalculator(calc, log="socketio.log", port=port) as calc:
     atoms.calc = calc
     md.run(steps=2)
