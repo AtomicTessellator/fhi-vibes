76ab2eae635a942c88a0902603bce9653df5ee87
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2018-11-08 13:40:19 +0100

    fireworks updated

--- examples/fireworks/fireworks_workflow_gen_example.py
+++ examples/fireworks/fireworks_workflow_gen_example.py
@@ -1,22 +1,18 @@
+'''Example for generating/submitting a full relaxation->full phonon workflow'''
 from argparse import ArgumentParser
-import numpy as np
-import os
 
-from fireworks import Firework, PyTask, Workflow
-from fireworks.core.rocket_launcher import launch_rocket
-
-from hilde.fireworks_api_adapter.launchpad import LaunchPad_hilde
-from hilde.fireworks_api_adapter.qlaunch_remote import qlaunch_remote
-from hilde.fireworks_api_adapter.rocket_launcher import rapidfire
+from hilde.fireworks_api_adapter.launchpad import LaunchPadHilde
+from hilde.fireworks_api_adapter.combined_launcher import rapidfire
 from hilde.helpers.brillouinzone import get_bands_and_labels
 from hilde.helpers.hash import hash_atoms
-from hilde.helpers.paths import cwd
-from hilde.helpers.utility_functions import get_smatrix, setup_workdir
+from hilde.helpers.utility_functions import get_smatrix
 from hilde.parsers import read_structure
 from hilde.phonon_db.phonon_db import connect
-from hilde.workflows.relax_phonopy import (gen_relax_phonopy_wf, aims_kgrid_conv_settings,
-                                           aims_relax_settings_light, aims_relax_settings_tight,
-                                           aims_force_settings)
+from hilde.workflows.relax_phonopy import gen_relax_phonopy_wf
+from default_aims_settings import (aims_kgrid_conv_settings,
+                                   aims_relax_settings_light,
+                                   aims_relax_settings_tight,
+                                   aims_force_settings)
 
 parser = ArgumentParser()
 parser.add_argument("-rh", "--remote_host", nargs="*",
@@ -38,10 +34,11 @@ parser.add_argument("-wd", "--workdir", default=".",
                     help="directory used to calculate the individual atom calculations")
 parser.add_argument("--no_kerberos", action="store_true",
                     help="If set do not use gss_api authentication")
-parser.add_argument('-g', '--geometry', default='geometry.in',
+parser.add_argument('-g', '--geometry', default='si.in',
                     help="The input geometry file")
 args = parser.parse_args()
 
+# Modifications of settings fore remote environment
 if args.remote_species_dir:
     aims_kgrid_conv_settings["species_dir"] = args.remote_species_dir
     aims_relax_settings_light["species_dir"] = args.remote_species_dir
@@ -53,40 +50,17 @@ if args.remote_command:
     aims_relax_settings_tight["aims_command"] = args.remote_command
     aims_force_settings["aims_command"] = args.remote_command
 
+# Get aims settings
 atoms = read_structure(args.geometry)
 atoms_hash, _ = hash_atoms(atoms)
 smatrix = get_smatrix(atoms, n_target=64)
-launchpad = LaunchPad_hilde()
-try:
-    query = {"name": f"{atoms.get_chemical_formula()}_{atoms_hash}", "state": "COMPLETED"}
-    wf_ids = launchpad.get_wf_ids(query=query, limit=100)
-    for wf_id in wf_ids:
-        launchpad.delete_wf(wf_id)
-except:
-  pass
-try:
-    query = {"name": f"{atoms.get_chemical_formula()}_{atoms_hash}", "state": "FIZZLED"}
-    wf_ids = launchpad.get_wf_ids(query=query, limit=100)
-    for wf_id in wf_ids:
-        launchpad.delete_wf(wf_id)
-except:
-    pass
-# try:
-#     query = {"name": f"{atoms.get_chemical_formula()}_{atoms_hash}", "state": "READY"}
-#     wf_ids = launchpad.get_wf_ids(query=query, limit=100)
-#     for wf_id in wf_ids:
-#         launchpad.delete_wf(wf_id)
-# except:
-#     pass
-try:
-    query = {"name": f"{atoms.get_chemical_formula()}_{atoms_hash}", "state": "RESERVED"}
-    wf_ids = launchpad.get_wf_ids(query=query, limit=100)
-    for wf_id in wf_ids:
-        launchpad.delete_wf(wf_id)
-except:
-    pass
+launchpad = LaunchPadHilde()
+# Clean up launchpad
+launchpad.clean_up_wflow(f"{atoms.get_chemical_formula()}_{atoms_hash}")
+
 wf = gen_relax_phonopy_wf(args.geometry,
                           "postgresql://hilde:hilde@130.183.206.193:5432/phonopy_db",
+                          "postgresql://hilde:hilde@localhost:5432/phonopy_db",
                           f"{atoms.get_chemical_formula()}_{atoms_hash}",
                           args.workdir,
                           "atoms_cur",
@@ -95,24 +69,25 @@ wf = gen_relax_phonopy_wf(args.geometry,
                           relax_light=aims_relax_settings_light,
                           relax_tight=aims_relax_settings_tight,
                           force_calc=aims_force_settings,
-                          spec_qad_kgrid={"_queueadapter" : {"walltime" : "00:10:00"} },
-                          spec_qad_relax={"_queueadapter" : {"walltime" : "00:10:00"} },
-                          spec_qad_forces={"_queueadapter" : {"nodes" : 3,"walltime" : "00:10:00"} })
+                          spec_qad_kgrid={"_queueadapter": {"walltime": "00:10:00"}},
+                          spec_qad_relax={"_queueadapter": {"walltime": "00:10:00"}},
+                          spec_qad_forces={"_queueadapter": {"nodes": 3, "walltime": "00:10:00"}})
 
 launchpad.add_wf(wf)
-qlaunch_remote("rapidfire", maxjobs_queue=250, nlaunches=0, remote_host=args.remote_host,
-               remote_user=args.remote_user, remote_password=args.remote_password,
-               remote_config_dir=["/u/tpurcell/git/hilde/examples/fireworks"], reserve=True,
-               gss_auth=not args.no_kerberos, wflow=wf)
+rapidfire(launchpad, launch_dir='.', nlaunches=0, njobs_queue=250, wflow=wf, njobs_block=500,
+          sleep_time=None, reserve=True, remote_host=args.remote_host,
+          remote_user=args.remote_user, remote_password=args.remote_password,
+          remote_config_dir=["/u/tpurcell/git/hilde/examples/fireworks"],
+          gss_auth=not args.no_kerberos)
 
 
-# db_path = "postgresql://hilde:hilde@localhost:5432/phonopy_db"
-# db = connect(db_path)
-# phonon = db.get_phonon(1e-5, selection=[("supercell_matrix", "=", smatrix),
-#                                   ("original_atoms_hash", "=", atoms_hash),
-#                                   ("has_fc", "=", True)])
-# bands, labels = get_bands_and_labels(atoms)
-# phonon.set_band_structure(bands)
-# plt = phonon.plot_band_structure(labels=labels)
-# plt.ylabel('Frequency [THz]')
-# plt.savefig('phonon_dispersion.pdf')
+db_path = "postgresql://hilde:hilde@localhost:5432/phonopy_db"
+db = connect(db_path)
+phonon = db.get_phonon(1e-5, selection=[("supercell_matrix", "=", smatrix),
+                                        ("original_atoms_hash", "=", atoms_hash),
+                                        ("has_fc", "=", True)])
+bands, labels = get_bands_and_labels(atoms)
+phonon.set_band_structure(bands)
+plt = phonon.plot_band_structure(labels=labels)
+plt.ylabel('Frequency [THz]')
+plt.savefig('phonon_dispersion.pdf')
