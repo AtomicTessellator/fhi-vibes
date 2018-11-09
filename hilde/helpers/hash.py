24f039ff1108c5322fbfc253aebf65cc49ab3cb6
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-09 09:31:37 +0100

    more human readble names

--- hilde/helpers/hash.py
+++ hilde/helpers/hash.py
@@ -6,36 +6,46 @@ from pathlib import Path
 from configparser import ConfigParser
 import json
 
-def hashfunc(string, default=''):
+
+def get_json(obj):
+    "Return json representation of obj"
+    return json.dumps(obj, cls=MyEncoder, sort_keys=True)
+
+
+def hashfunc(string, default=""):
     """ Wrap the sha hash function and check for empty objects """
-    if (string == ''
-        or string == '[]'
-        or string == '{}'
-        or string == 'None'):
-        string = ''
-    return hash_sha(string.encode('utf8'))
+    if string == "" or string == "[]" or string == "{}" or string == "None":
+        string = ""
+    return hash_sha(string.encode("utf8"))
+
 
 def atoms2json(atoms, ignore_atoms=[], ignore_calc=[]):
     atomsdict = atoms2dict(atoms)
-    for name in ['unique_id'] + ignore_atoms:
+
+    for name in ["unique_id"] + ignore_atoms:
         atomsdict.pop(name)
     calcdict = {}
 
     # clean calculator entries
-    if 'calculator_parameters' in atomsdict:
-        cp = atomsdict['calculator_parameters']
-        for name in [k for k in cp if k in ignore_calc]:
-            cp.pop(name)
-        if 'species_dir' in cp:
-            cp['species_dir'] = Path(cp['species_dir']).parts[-1]
+    if "calculator_parameters" in atomsdict:
+        calculator_params = atomsdict["calculator_parameters"]
+        for key in calculator_params:
+            if key in ignore_calc:
+                calculator_params.pop(key)
+
+        if "species_dir" in calculator_params:
+            calculator_params["species_dir"] = Path(
+                calculator_params["species_dir"]
+            ).parts[-1]
 
     for name in all_properties:
         atomsdict.pop(name, None)
-    for name in ['calculator', 'calculator_parameters']:
+
+    for name in ["calculator", "calculator_parameters"]:
         if name in atomsdict:
             calcdict[name] = atomsdict.pop(name)
-    getjson = lambda obj: json.dumps(obj, cls=MyEncoder, sort_keys=True)
-    return getjson(atomsdict), getjson(calcdict)
+
+    return get_json(atomsdict), get_json(calcdict)
 
 
 def hash_atoms(atoms, ignore_atoms=[], ignore_calc=[], ignore_file=None):
@@ -43,12 +53,24 @@ def hash_atoms(atoms, ignore_atoms=[], ignore_calc=[], ignore_file=None):
     if ignore_file is not None:
         fil = Path(ignore_file)
         if fil.exists():
-            cg = ConfigParser()
-            cg.read(fil)
-            ignore_atoms = [k for k in cg['atoms'] if not cg.getboolean('atoms', k)]
-            ignore_calc = [k for k in cg['calculator'] if not cg.getboolean('calculator', k)]
+            configparser = ConfigParser()
+            configparser.read(fil)
+
+            ignore_atoms = [
+                key
+                for key in configparser["atoms"]
+                if not configparser.getboolean("atoms", key)
+            ]
+
+            ignore_calc = [
+                key
+                for key in configparser["calculator"]
+                if not configparser.getboolean("calculator", key)
+            ]
 
     atomsjson, calcjson = atoms2json(atoms, ignore_atoms, ignore_calc)
+
     atomshash = hashfunc(atomsjson).hexdigest()
     calchash = hashfunc(calcjson).hexdigest()
+
     return atomshash, calchash
