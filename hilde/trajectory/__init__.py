d48d124b16027640fb9b927ba4164809cba15056
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-13 10:57:41 +0100

    mv step2dict etc. into hilde/trajectory and update yaml trajectory example

---
+++ hilde/trajectory/__init__.py
@@ -0,0 +1,53 @@
+""" tools for storing MD trajectories """
+
+import numpy as np
+from ase import units as u
+from hilde.helpers.fileformats import to_yaml, from_yaml
+
+
+def step2dict(atoms, md):
+    """ extract information from md step and convet to plain dict """
+
+    # info from MD algorithm
+    md_dict = {"nsteps": md.nsteps, "dt": md.dt}
+
+    # structure
+    atoms_dict = {"positions": atoms.positions.tolist()}
+    # if periodic system, append lattice
+    if any(atoms.pbc):
+        atoms_dict.update({"cell": atoms.cell.tolist()})
+
+    # calculated values
+    calc_dict = {}
+    calc = atoms.calc
+    # convert numpy arrays into ordinary lists
+    for key, val in calc.results.items():
+        if isinstance(val, np.ndarray):
+            calc_dict[key] = val.tolist()
+        else:
+            calc_dict[key] = val
+
+    return {"MD": md_dict, "atoms": atoms_dict, "calculator": calc_dict}
+
+
+def metadata2dict(atoms, calc, md):
+    """ convert metadata information to plain dict """
+
+    md_dict = md.todict()
+    # save time and mass unit
+    md_dict.update({"fs": u.fs, "dt": md.dt, "kg": u.kg})
+
+    # structure
+    atoms_dict = {
+        "numbers": atoms.numbers.tolist(),
+        "symbols": [f"{sym}" for sym in atoms.symbols],
+        "masses": md.masses.T.tolist()[0],
+        "positions": atoms.positions.tolist(),
+    }
+    # if periodic system, append lattice
+    if any(atoms.pbc):
+        atoms_dict.update({"cell": atoms.cell.tolist()})
+
+    calc_dict = {"name": calc.__class__.__name__, "params": calc.todict()}
+
+    return {"MD": md_dict, "atoms": atoms_dict, "calculator": calc_dict}
