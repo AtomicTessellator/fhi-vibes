5491590f5b0ac812bae494ac239d4a75b8bacd47
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-15 18:53:11 +0100

    move general part of step2dict/metadata2dict to trajectory/__init__

--- hilde/trajectory/md.py
+++ hilde/trajectory/md.py
@@ -8,6 +8,7 @@ Logic:
 
 import numpy as np
 from ase import units as u
+from . import metadata2dict as _metadata2dict, step2dict as _step2dict
 from hilde.helpers.fileformats import to_yaml, from_yaml, last_from_yaml
 
 
@@ -31,28 +32,7 @@ def step2dict(atoms, calc, md, NPT=False):
     # info from MD algorithm
     md_dict = {"nsteps": md.nsteps, "dt": md.dt}
 
-    # structure
-    atoms_dict = {"positions": atoms.positions.tolist()}
-
-    if atoms.get_velocities() is not None:
-        atoms_dict.update({"velocities": atoms.get_velocities().tolist()})
-
-    # if periodic system, append lattice
-    if NPT and any(atoms.pbc):
-        atoms_dict.update({"cell": atoms.cell.tolist()})
-
-    # calculated values
-    calc_dict = {}
-    # convert numpy arrays into ordinary lists
-    for key, val in calc.results.items():
-        if isinstance(val, np.ndarray):
-            calc_dict[key] = val.tolist()
-        elif isinstance(val, np.float):
-            calc_dict[key] = float(val)
-        else:
-            calc_dict[key] = val
-
-    return {"MD": md_dict, "atoms": atoms_dict, "calculator": calc_dict}
+    return {"MD": md_dict, **_step2dict(atoms, calc, append_cell=NPT)}
 
 
 def metadata2dict(atoms, calc, md):
@@ -62,22 +42,4 @@ def metadata2dict(atoms, calc, md):
     # save time and mass unit
     md_dict.update({"fs": u.fs, "dt": md.dt, "kg": u.kg})
 
-    # structure
-    atoms_dict = {
-        "symbols": [f"{sym}" for sym in atoms.symbols],
-        "masses": md.masses.T.tolist()[0],
-        "positions": atoms.positions.tolist(),
-    }
-
-    # if periodic system, append lattice
-    if any(atoms.pbc):
-        atoms_dict.update({"cell": atoms.cell.tolist()})
-
-    params = calc.todict()
-    for key, val in params.items():
-        if isinstance(val, tuple):
-            params[key] = list(val)
-
-    calc_dict = {"name": calc.__class__.__name__, "params": params}
-
-    return {"MD": md_dict, "atoms": atoms_dict, "calculator": calc_dict}
+    return {"MD": md_dict, **_metadata2dict(atoms, calc)}
