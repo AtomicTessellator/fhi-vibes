a91048f8ab115315fb206c62551b42be75353ff5
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-15 16:06:35 +0100

    minimal cosmetics

--- examples/phonons/phonons_socketio.py
+++ examples/phonons/phonons_socketio.py
@@ -1,90 +1,87 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from pathlib import Path
 from time import time
+from pathlib import Path
+import matplotlib.pyplot as plt
 
-from ase.calculators.aims import Aims
-from ase.calculators.socketio import SocketIOCalculator
 from hilde.parsers import read_structure
 from hilde.helpers.supercell import make_cubic_supercell
-from hilde.helpers import cwd, d2k, get_cubicness
+from hilde.helpers import d2k
 from hilde.phonopy import phono as ph
-from hilde.tasks.calculate import calculate, calculate_multiple_socketio
-from hilde.settings import Settings
+from hilde.tasks.calculate import calculate_multiple_socketio
 from hilde.templates.aims import setup_aims
 import hilde.helpers.brillouinzone as bz
 
-atoms = read_structure('../si.in')
+atoms = read_structure("../si.in")
 vol = atoms.get_volume()
 
-base_folder = Path(f'{atoms.sysname}').absolute()
+base_folder = Path(f"{atoms.sysname}").absolute()
 
 port = 31415
 k_grid = d2k(atoms, 1)
+n_target = 4
 
-print(f'k_grid primitive cell: {k_grid}')
+print(f"k_grid primitive cell: {k_grid}")
 
-relax_folder = base_folder / f'relax_{vol:.3f}'
+relax_folder = base_folder / f"relax_{vol:.3f}"
 
 try:
-    ratoms = read_structure(relax_folder / 'geometry.in.next_step')
+    ratoms = read_structure(relax_folder / "geometry.in.next_step")
 except FileNotFoundError:
     ratoms = atoms
 
-n_target = 4
 sc, smatrix = make_cubic_supercell(ratoms, target_size=n_target)
 
 print(sc.cell)
 
 phonon, sc, scs = ph.preprocess(ratoms, smatrix.T)
 
-workdir = base_folder / '{}{}{}_{}{}{}_{}{}{}_{:.3f}'.format(
-    *smatrix.flatten(), vol)
+workdir = base_folder / "{}{}{}_{}{}{}_{}{}{}_{:.3f}".format(*smatrix.flatten(), vol)
 workdir.mkdir(exist_ok=True, parents=True)
 
-sc.write(workdir / 'supercell.in')
+sc.write(workdir / "supercell.in")
 
 k_grid = d2k(sc, 1)
-print(f'k_grid supercell: {k_grid}')
+print(f"k_grid supercell: {k_grid}")
 
 # set the phonon object
 phonon, sc, scs = ph.preprocess(ratoms, smatrix.T)
 
-print(f'{len(scs)} supercells created.')
+print(f"{len(scs)} supercells created.")
 
 socketio_settings = {
-    'use_pimd_wrapper': ('localhost', port),
-    'compute_forces': True,
-    'sc_accuracy_rho': 1e-4,
-    'k_grid': k_grid
+    "use_pimd_wrapper": ("localhost", port),
+    "compute_forces": True,
+    "sc_accuracy_rho": 1e-4,
+    "k_grid": k_grid,
 }
 
 aims = setup_aims(custom_settings=socketio_settings)
 
-phonopy_log = workdir / 'phonopy.log'
-trajectory = workdir / 'phonopy.traj'
-tmp_dir = workdir / 'socketio'
+phonopy_log = workdir / "phonopy.log"
+trajectory = workdir / "phonopy.traj"
+tmp_dir = workdir / "socketio"
 
 stime = time()
-cells_computed = calculate_multiple_socketio(cells=scs,
-                                             calculator=aims,
-                                             workdir=tmp_dir,
-                                             port=port,
-                                             trajectory=trajectory,
-                                             force=False,
-                                             log_file=phonopy_log)
+cells_computed = calculate_multiple_socketio(
+    cells=scs,
+    calculator=aims,
+    workdir=tmp_dir,
+    port=port,
+    trajectory=trajectory,
+    force=False,
+    log_file=phonopy_log,
+)
 timing_socket_io = time() - stime
-print(f'.. done in {timing_socket_io:.2f}s')
+print(f".. done in {timing_socket_io:.2f}s")
 
 force_sets = [atoms.get_forces() for atoms in cells_computed]
 phonon.produce_force_constants(force_sets)
 
 dos = ph.get_dos(phonon)
 plt.plot(dos[0], dos[1])
-plt.savefig(workdir / 'dos.pdf')
+plt.savefig(workdir / "dos.pdf")
 
-#bands
+# bands
 bands, labels = bz.get_bands_and_labels(phonon.primitive)
 phonon.set_band_structure(bands)
 phonon.plot_band_structure(labels=labels)
-plt.savefig(workdir / 'bandstructure.pdf')
+plt.savefig(workdir / "bandstructure.pdf")
