fc74d248f0a396a10f12743f8cdd6dc9e862a456
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-20 15:40:48 +0100

    MD | introduce workflow with restart, logging, etc.

--- hilde/molecular_dynamics/__init__.py
+++ hilde/molecular_dynamics/__init__.py
@@ -1,14 +1,52 @@
 """ run molecular dynamics simulations using the ASE classe """
 
+import tarfile
 from pathlib import Path
-from warnings import warn
+from subprocess import run
+from ase import units as u
 from ase.calculators.socketio import SocketIOCalculator
 from hilde.trajectory.md import step2file, metadata2file, last_from_yaml
 from hilde.watchdogs import WallTimeWatchdog as Watchdog
 from hilde.helpers.paths import cwd
+from hilde.helpers.compression import backup_folder
 
 
-def prepare_from_trajectory(atoms, md, trajectory="trajectory.yaml"):
+def setup_md(
+    atoms,
+    algorithm="Verlet",
+    temperature=None,
+    timestep=None,
+    friction=None,
+    logfile=None,
+    trajectory="trajectory.yaml",
+    **kwargs,
+):
+    """ create and ase.md object with respective settings """
+
+    temp = temperature * u.fs
+    dt = timestep * u.fs
+
+    if "verlet" in algorithm.lower():
+        from ase.md.verlet import VelocityVerlet
+
+        md = VelocityVerlet(atoms, timestep=dt, logfile=logfile)
+
+    elif "langevin" in algorithm.lower():
+        from ase.md.langevin import Langevin
+
+        md = Langevin(
+            atoms, temperature=temp, timestep=dt, friction=friction, logfile=logfile
+        )
+
+    else:
+        raise RuntimeError(f"Molecular dynamics mode {algorithm} is not suppported.")
+
+    prepared = prepare_from_trajectory(atoms, md, trajectory)
+
+    return atoms, md, prepared
+
+
+def prepare_from_trajectory(atoms, md, trajectory="trajectory.yaml", **kwargs):
     """ Take the last step from trajectory and initialize atoms + md accordingly """
 
     trajectory = Path(trajectory).absolute()
@@ -18,8 +56,8 @@ def prepare_from_trajectory(atoms, md, trajectory="trajectory.yaml"):
 
         atoms.set_positions(last_atoms["atoms"]["positions"])
         atoms.set_velocities(last_atoms["atoms"]["velocities"])
-    else:
-        warn(f"{trajectory} does  not exist, nothing to prepare")
+        return True
+    print(f"** {trajectory} does  not exist, nothing to prepare")
 
 
 def run_md(
@@ -32,6 +70,9 @@ def run_md(
     socketio_port=None,
     walltime=1800,
     workdir=".",
+    logfile=None,
+    restart_cmd=None,
+    **kwargs,
 ):
     """ run and MD for a specific time
 
@@ -55,6 +96,14 @@ def run_md(
         socket_calc = calc
     atoms.calc = calc
 
+    # backup
+    additional_files = []
+    if logfile is not None:
+        additional_files = [logfile]
+
+    if Path(workdir).exists():
+        backup_folder(workdir, additional_files=additional_files)
+
     with SocketIOCalculator(socket_calc, port=socketio_port) as iocalc, cwd(
         workdir, mkdir=True
     ):
@@ -70,8 +119,16 @@ def run_md(
         # store MD metadata locally
         metadata2file(atoms, calc, md, file=metadata)
 
-        while watchdog() == False and md.nsteps < maxsteps:
+        while not watchdog() and md.nsteps < maxsteps:
             md.run(1)
             step2file(atoms, atoms.calc, md, trajectory)
 
-    return md.nsteps >= maxsteps
+    # restart
+    if md.nsteps < maxsteps:
+        if restart_cmd is not None:
+            print(f"restart with {restart_cmd}")
+            run(restart_cmd.split())
+    else:
+        # final backup
+        backup_folder(workdir, additional_files=additional_files)
+        return True
