e8dd2bc364693bcde9395ceca6a2c18d84adf658
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-20 17:20:19 +0100

    update settings, use python 3.6 per default

--- hilde/settings/__init__.py
+++ hilde/settings/__init__.py
@@ -1,24 +1,16 @@
-from hilde.helpers.config import AttributeDict, ConfigDict
+""" Settings class for holding settings, based on configparser.ConfigParser """
+from hilde.helpers.config import ConfigDict
 from hilde.helpers.hash import hashfunc
+
 
 class Settings(ConfigDict):
     """ Class to hold the settings parsed from highaims.cfg (or similar)"""
-    def __init__(self, config_files='highaims.cfg'):
+
+    def __init__(self, config_files="highaims.cfg"):
         super().__init__(config_files=config_files)
-        if "fw_settings" in self:
-            self.fw_settings.remote_host = list(self.fw_settings.remote_host)
-            self.fw_settings.remote_config_dir = list(self.fw_settings.remote_config_dir)
-            self.fw_settings.tasks2queue = list(self.fw_settings.tasks2queue)
 
-
-    def get_hash(self, short = True):
+    def get_hash(self):
         return hashfunc(self.get_string())
-
-    def for_handler(self, choose=None):
-        handler_setup = {**self.machine, **self.watchdog, **self.database, 'k_grid': self.dft.k_grid}
-        if choose:
-            handler_setup = {**handler_setup, **self[choose]}
-        return AttributeDict(handler_setup)
 
     def print(self):
         print(self.get_string())
