3d8c4d980409d629b9b4c7ee1f1867ac08848687
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-21 15:54:27 +0100

    allow backup() to store backup in a specific folder

--- hilde/helpers/compression.py
+++ hilde/helpers/compression.py
@@ -2,6 +2,7 @@
 
 import tarfile
 from pathlib import Path
+from hilde.helpers import cwd
 
 
 def backup_filename(workdir="."):
@@ -16,12 +17,16 @@ def backup_filename(workdir="."):
     return file(counter)
 
 
-def backup_folder(source_dir, additional_files=[]):
+def backup_folder(source_dir, target_folder=".", additional_files=[]):
     """ backup a folder as .tgz """
 
-    output_filename = backup_filename()
+    output_filename = backup_filename(target_folder)
 
-    make_tarfile(output_filename, source_dir, additional_files=additional_files)
+    make_tarfile(
+        output_filename,
+        source_dir,
+        additional_files=additional_files,
+    )
 
 
 def make_tarfile(output_filename, source_dir, additional_files=[], arcname=None):
