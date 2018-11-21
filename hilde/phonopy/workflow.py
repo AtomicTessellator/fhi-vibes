a07e016a2745bbef4a28351fccf7c23e4898fbdf
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-21 15:56:44 +0100

    phonopy | simple restarts!

--- hilde/phonopy/workflow.py
+++ hilde/phonopy/workflow.py
@@ -1,5 +1,6 @@
 """ Provide a full highlevel phonopy workflow """
 
+import sys
 from pathlib import Path
 
 from ase.calculators.socketio import SocketIOCalculator
@@ -7,9 +8,26 @@ from ase.calculators.socketio import SocketIOCalculator
 from hilde.helpers.k_grid import update_k_grid
 from hilde.helpers.paths import cwd
 import hilde.phonopy.wrapper as ph
-from hilde.trajectory.phonopy import metadata2file, step2file
+from hilde.trajectory.phonopy import metadata2file, step2file, last_from_yaml
 from hilde.watchdogs import WallTimeWatchdog as Watchdog
+from hilde.helpers.compression import backup_folder
 from .postprocess import postprocess
+
+
+_calc_dirname = "calculations"
+
+
+def last_calculation_id(trajectory):
+    """ return the id of the last computed supercell """
+    disp_id = -1
+
+    try:
+        dct = last_from_yaml(trajectory)
+        disp_id = dct["phonopy"]["id"]
+    except (FileNotFoundError, KeyError):
+        pass
+
+    return disp_id
 
 
 def phonopy(
@@ -29,18 +47,24 @@ def phonopy(
     pickle_file="phonon.pick",
     db_path=None,
     backup_settings=None,
+    restart_cmd=None,
     **kwargs,
     #    fingerprint_file="fingerprint.dat",
 ):
     """ perform a full phonopy calculation """
 
     trajectory = Path(trajectory).absolute()
-    workdir = Path(workdir).absolute()
+    calc_dir = Path(workdir) / _calc_dirname
 
     watchdog = Watchdog(walltime=walltime, buffer=1)
 
+    if Path(workdir).exists():
+        print(f"***Caution, directory {workdir} exists, chance of data loss.")
+
+    # Phonopy preprocess
     phonon, supercell, scs = ph.preprocess(atoms, supercell_matrix, displacement)
 
+    # make sure forces are computed (aims only)
     if "compute_forces" in calc.parameters:
         calc.parameters["compute_forces"] = True
 
@@ -55,7 +79,7 @@ def phonopy(
     if kpt_density is not None:
         update_k_grid(calculation_atoms, calc, kpt_density)
 
-    # save input geometries
+    # save input geometries and configuration
     with cwd(workdir, mkdir=True):
         atoms.write(primitive_file, format="aims", scaled=True)
         supercell.write(supercell_file, format="aims", scaled=True)
@@ -64,10 +88,11 @@ def phonopy(
             backup_settings.write()
 
     # perform calculation
-    with cwd(workdir / "calculations", mkdir=True):
+    with cwd(calc_dir, mkdir=True):
         with SocketIOCalculator(socket_calc, port=socketio_port) as iocalc:
             # save inputs and supercell
-            metadata2file(atoms, calc, phonon, trajectory)
+            if last_calculation_id(trajectory) < 0:
+                metadata2file(atoms, calc, phonon, trajectory)
 
             if socketio_port is not None:
                 calc = iocalc
@@ -78,6 +103,10 @@ def phonopy(
                 if cell is None:
                     continue
 
+                # if precomputed:
+                if ii <= last_calculation_id(trajectory):
+                    continue
+
                 # update calculation_atoms and compute force
                 calculation_atoms.positions = cell.positions
                 _ = calculation_atoms.get_forces()
@@ -85,7 +114,16 @@ def phonopy(
                 step2file(calculation_atoms, calculation_atoms.calc, ii, trajectory)
 
                 if watchdog():
-                    raise ResourceWarning("**Watchdog: running out of time!")
+                    break
+
+    # backup and restart if necessary
+    if ii < len(scs) - 1:
+        backup_folder(calc_dir, target_folder=workdir)
+        if restart_cmd is not None:
+            print(f"restart with {restart_cmd}")
+            run(restart_cmd.split())
+        else:
+            sys.exit("**Watchdog: running out of time! No restart script.")
 
     postprocess(
         phonon,
