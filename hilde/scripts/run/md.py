106f34a104e6f081fcbb23a4a8e5be5791390e93
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-22 16:23:26 +0100

    remove unnecessary layer from run functions

--- hilde/scripts/run/md.py
+++ hilde/scripts/run/md.py
@@ -11,36 +11,34 @@ from hilde.templates.aims import setup_aims
 from hilde.molecular_dynamics import run_md, setup_md
 
 
-def run(atoms, calc, settings):
-    "run MD"
+settings = Settings(default_config_name)
 
-    converged = run_md(atoms, calc, socketio_port=settings.socketio.port, **settings.md)
+atoms = read("geometry.in", format="aims")
+calc = setup_aims(settings=settings)
 
-    return converged
-
-
-if __name__ == "__main__":
-
-    settings = Settings(default_config_name)
-
-    atoms = read('geometry.in', format='aims')
-    calc = setup_aims(settings=settings)
-
-    if not Path('trajectory.yaml').exists():
-        if 'force_constants' in settings.md:
-            print('Initialize positions and velocities using force constants.')
-            force_constants = np.loadtxt(settings.md.force_constants)
-            PhononHarmonics(atoms, force_constants, quantum=False, temp=settings.md.temperature * units.kB)
-        else:
-            print('Initialize velocities according to Maxwell-Botlzmann distribution.')
-            MaxwellBoltzmannDistribution(atoms, temp=settings.md.temperature * units.kB)
-
-    converged = run(atoms, calc, settings)
-
-    if not converged:
-        if "restart" in settings:
-            sp.run(settings.restart.command.split())
-        else:
-            print("Task not completed, please inspect and rerun.")
+# MD initialization
+if not Path("trajectory.yaml").exists():
+    if "force_constants" in settings.md:
+        print("Initialize positions and velocities using force constants.")
+        force_constants = np.loadtxt(settings.md.force_constants)
+        PhononHarmonics(
+            atoms,
+            force_constants,
+            quantum=False,
+            temp=settings.md.temperature * units.kB,
+        )
     else:
-        print('done.')
+        print("Initialize velocities according to Maxwell-Botlzmann distribution.")
+        MaxwellBoltzmannDistribution(atoms, temp=settings.md.temperature * units.kB)
+
+
+converged = run_md(atoms, calc, socketio_port=settings.socketio.port, **settings.md)
+
+
+if not converged:
+    if "restart" in settings:
+        sp.run(settings.restart.command.split())
+    else:
+        print("Task not completed, please inspect and rerun.")
+else:
+    print("done.")
--- hilde/scripts/run/phonopy.py
+++ hilde/scripts/run/phonopy.py
@@ -7,34 +7,25 @@ from hilde.settings import Settings, default_config_name
 from hilde.templates.aims import setup_aims
 
 
-def run(atoms, calc, settings):
-    "run phonopy"
+settings = Settings(default_config_name)
+
+atoms = read("geometry.in", format="aims")
+calc = setup_aims(settings=settings)
 
 
-    completed = phonopy(
-        atoms,
-        calc,
-        socketio_port=settings.socketio.port,
-        kpt_density=settings.control_kpt.density,
-        **settings.phonopy
-    )
-
-    return completed
+completed = phonopy(
+    atoms,
+    calc,
+    socketio_port=settings.socketio.port,
+    kpt_density=settings.control_kpt.density,
+    **settings.phonopy
+)
 
 
-if __name__ == "__main__":
-
-    settings = Settings(default_config_name)
-
-    atoms = read("geometry.in", format="aims")
-    calc = setup_aims(settings=settings)
-
-    completed = run(atoms, calc, settings)
-
-    if not completed:
-        if "restart" in settings:
-            sp.run(settings.restart.command.split())
-        else:
-            print("Task not completed, please inspect and rerun.")
+if not completed:
+    if "restart" in settings:
+        sp.run(settings.restart.command.split())
     else:
-        print('done.')
+        print("Task not completed, please inspect and rerun.")
+else:
+    print("done.")
