f376d83037b876bfcfa724be68db0c554133e108
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-22 10:58:27 +0100

    update examples

--- examples/md/run_md.py
+++ examples/md/run_md.py
@@ -8,28 +8,31 @@ from hilde.helpers.k_grid import update_k_grid
 from hilde.molecular_dynamics import run_md, setup_md
 
 
-def run(atoms, config_files):
+def run(atoms, settings):
     "run and MD"
-    settings = Settings(config_files=config_files)
 
     calc = setup_aims(settings=settings)
 
     update_k_grid(atoms, calc, settings.control_kpt.density)
 
-    atoms, md, prepared = setup_md(atoms, **settings.md)
+    MaxwellBoltzmannDistribution(atoms, temp=settings.md.temperature * units.kB)
 
-    if not prepared:
-        MaxwellBoltzmannDistribution(atoms, temp=settings.md.temperature * units.kB)
-
-    converged = run_md(
-        atoms, calc, md, socketio_port=settings.socketio.port, **settings.md
-    )
+    converged = run_md(atoms, calc, socketio_port=settings.socketio.port, **settings.md)
 
     return converged
 
 
 if __name__ == "__main__":
     atoms = read("si.in", format="aims")
-    config_files = ["hilde.cfg", "md.cfg"]
+    settings = Settings(["hilde.cfg", "md.cfg"])
 
-    run(atoms, config_files)
+    converged = run(atoms, settings)
+
+    if not converged:
+        from subprocess import run
+
+        print("Job not completed, restart.")
+        run(settings.restart.command.split())
+
+    print('done.')
+
