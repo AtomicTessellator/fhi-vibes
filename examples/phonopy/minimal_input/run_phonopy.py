a1aab39c0aef94940d781a0e0b7830696723417e
Flo-Think <florian_knoop@gmx.de>
2018-11-24 16:12:12 +0100

    update to have minimal example

--- examples/phonopy/minimal_input/run_phonopy.py
+++ examples/phonopy/minimal_input/run_phonopy.py
@@ -13,16 +13,11 @@ atoms, calc = setup_aims(settings=settings)
 completed = phonopy(
     atoms,
     calc,
-    socketio_port=settings.socketio.port,
-    kpt_density=settings.control_kpt.density,
     **settings.phonopy
 )
 
 
 if not completed:
-    if "restart" in settings:
-        sp.run(settings.restart.command.split())
-    else:
-        print("Task not completed, please inspect and rerun.")
+    print("Task not completed, please inspect and rerun.")
 else:
     print("done.")
