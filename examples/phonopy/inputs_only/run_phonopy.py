7f04a31821bb4748ececbf32bcd12aafb4c54b0e
Flo-Think <florian_knoop@gmx.de>
2018-11-24 16:06:23 +0100

    only inputs added to phonopy example

---
+++ examples/phonopy/inputs_only/run_phonopy.py
@@ -0,0 +1,28 @@
+import subprocess as sp
+
+from hilde.phonopy.workflow import phonopy
+from hilde.settings import Settings
+from hilde.templates.aims import setup_aims
+
+
+settings = Settings()
+
+atoms, calc = setup_aims(settings=settings)
+
+
+completed = phonopy(
+    atoms,
+    calc,
+    socketio_port=settings.socketio.port,
+    kpt_density=settings.control_kpt.density,
+    **settings.phonopy
+)
+
+
+if not completed:
+    if "restart" in settings:
+        sp.run(settings.restart.command.split())
+    else:
+        print("Task not completed, please inspect and rerun.")
+else:
+    print("done.")
