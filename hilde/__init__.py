c64ce49fc1838d6d48d60aa26fbb0f51d71b191c
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-11-24 15:07:15 +0100

    more DEFAULT_*_FILE constants, minor revision

--- hilde/__init__.py
+++ hilde/__init__.py
@@ -1,5 +1,10 @@
 """ general things """
 
+from pathlib import Path
+
 supported_tasks = ["relaxation", "phonopy", "md"]
 
-DEFAULT_CONFIG_FILE = "/".join(__file__.split("/")[:-2] + ["config/hilde.cfg"])
+DEFAULT_SETTINGS_FILE = "settings.in"
+DEFAULT_GEOMETRY_FILE = "geometry.in"
+DEFAULT_TEMP_SETTINGS_FILE = "temp_settings.in"
+DEFAULT_CONFIG_FILE = Path(__file__).parents[1] / "config" / "hilde.cfg"
