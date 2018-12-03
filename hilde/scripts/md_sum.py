142627fff0df4687bf1296109478fcf49773af1c
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-12-03 19:00:10 +0100

    md | make md_sum understand trajectories

--- hilde/scripts/md_sum.py
+++ hilde/scripts/md_sum.py
@@ -1,17 +1,12 @@
 """ Summarize output from ASE.md class (in md.log) """
 
+from pathlib import Path
 from argparse import ArgumentParser
 import numpy as np
+from hilde.trajectory import reader
 
 
-def main():
-    """ main routine """
-    parser = ArgumentParser(description="Read md.log and make simple statistics")
-    parser.add_argument("file", help="md.log input file")
-    parser.add_argument("-p", "--plot", action="store_true", help="plot to pdf")
-    parser.add_argument("--avg", type=int, help="running avg in plot", default=50)
-    args = parser.parse_args()
-
+def parse_log(filename):
     e_kin = []
     e_pot = []
     temp = []
@@ -28,6 +23,39 @@ def main():
             temp.append(T)
             e_kin.append(ek)
             e_pot.append(ep)
+
+    return e_kin, e_pot, temp, time
+
+
+def main():
+    """ main routine """
+    parser = ArgumentParser(description="Read md.log and make simple statistics")
+    parser.add_argument("file", help="md.log or trajectory.yaml input file")
+    parser.add_argument("-p", "--plot", action="store_true", help="plot to pdf")
+    parser.add_argument("--avg", type=int, help="running avg in plot", default=50)
+    parser.add_argument("-v", "--verbose", action="store_true", help="give more info")
+    args = parser.parse_args()
+
+    infile = Path(args.file)
+
+    if "yaml" in infile.suffix:
+        trajectory = reader(infile)[1:]
+        e_kin = [atoms.get_kinetic_energy() for atoms in trajectory]
+        e_pot = [atoms.get_potential_energy() for atoms in trajectory]
+        temp = [atoms.get_temperature() for atoms in trajectory]
+        time = [atoms.info["nsteps"] * atoms.info["dt"] * 0.001 for atoms in trajectory]
+    elif "log" in infile.suffix:
+        e_kin, e_pot, temp, time = parse_log(infile)
+
+    if args.verbose:
+        n_range = min(20, len(trajectory) - 2)
+        print(f"step   time   temperature")
+        for ii in range(n_range):
+            print(
+                f'{trajectory[-n_range+ii].info["nsteps"]:5d} '
+                f"{time[-n_range+ii]:10.4f} "
+                f"{temp[-n_range+ii]:10.4f} "
+            )
 
     print(f"Simulation time:        {time[-1] - time[0]:.4f} ps ({len(time)} steps)")
     print(f"Temperature:            {np.mean(temp):.2f} +/- {np.std(temp):.2f}K")
