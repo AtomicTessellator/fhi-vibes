12e2298302bf858e081f7d270a467d9818601bce
Florian Knoop <knoop@fhi-berlin.mpg.de>
2018-12-07 12:04:46 +0100

    remove unnecessary imports in phonon_db example

--- examples/phonon_db/phonon_db.py
+++ examples/phonon_db/phonon_db.py
@@ -2,28 +2,15 @@
 
 import numpy as np
 
-from ase.calculators.emt import EMT
-from ase.build import bulk
-import importlib as il
-from phonopy import Phonopy
-
-import pickle
-
+from hilde.helpers.pickletools import pread
 from hilde.helpers.hash import hash_atoms_and_calc
-from hilde.helpers.brillouinzone import get_bands
-from hilde.helpers.supercell import make_cubic_supercell
 from hilde.phonon_db.phonon_db import connect
-from hilde.phonopy import wrapper as ph
-from hilde.tasks.calculate import calculate_multiple
-from hilde.phono3py import wrapper as ph3
-from hilde.structure.convert import to_Atoms
-from hilde.structure.misc import get_sysname
 
 # Connect to the database
 db = connect("test.db")
 
 # Load the atoms and phonopy objects from the pick file
-atoms, phonon = pickle.load(open('phonopy.pick', 'rb'))
+atoms, phonon = pread("phonopy.pick")
 
 # Get the hashes of the atoms object for easy structure comparison
 atoms_hash, calc_hash = hash_atoms_and_calc(atoms)
@@ -62,5 +49,11 @@ row = list(
 )[0]
 
 # Compare the second order force constants and the Helmholtz free energies of the phonopy and row objects
-print("Both force constant matrices are the same:", np.all(row.fc_2[:] == phonon.get_force_constants()[:]))
-print("All free energy values are the same:", np.all(row.tp_A == phonon.get_thermal_properties()[1]))
+print(
+    "Both force constant matrices are the same:",
+    np.all(row.fc_2[:] == phonon.get_force_constants()[:]),
+)
+print(
+    "All free energy values are the same:",
+    np.all(row.tp_A == phonon.get_thermal_properties()[1]),
+)
