783a708a259fff1701a1a1acc6d7dac40da3bdcc
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2018-12-07 14:31:42 +0100

    phonons_fireworks.py and settings problems fixed

--- examples/phonopy/fireworks/phonons_fireworks.py
+++ examples/phonopy/fireworks/phonons_fireworks.py
@@ -2,12 +2,15 @@ from ase.io import read
 
 from fireworks import Workflow
 
+from pathlib import Path
+
 from hilde import DEFAULT_CONFIG_FILE
 from hilde.fireworks_api_adapter.launchpad import LaunchPadHilde
 from hilde.settings import Settings
 from hilde.workflows.workflow_generator import generate_workflow
 
 settings = Settings()
+settings.phonopy.workdir = str(Path(settings.phonopy.workdir).absolute())
 atoms = read(settings.geometry.file)
 
 launchpad = LaunchPadHilde.from_file("/home/purcell/.fireworks/my_launchpad.yaml")
