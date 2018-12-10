4357cc460ac0125d026ad896b3602863b3a5f905
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2018-12-10 19:35:18 +0100

    fixing fireworks test.py

--- examples/fireworks/fireworks_example.py
+++ examples/fireworks/fireworks_example.py
@@ -10,7 +10,6 @@ from hilde.helpers.hash import hash_atoms_and_calc
 from hilde.helpers.paths import cwd
 from hilde.helpers.utility_functions import get_smatrix
 from hilde.phonon_db.phonon_db import connect
-from hilde.structure.structure import pAtoms
 from hilde.workflows.workflow_generator import generate_firework
 
 from fireworks import Workflow
@@ -18,7 +17,7 @@ from fireworks import Workflow
 db_name = (os.getcwd() + '/test.db')
 print(f'database: {db_name}')
 
-atoms = pAtoms(bulk('Ni', 'fcc', a=3.5))
+atoms = bulk('Ni', 'fcc', a=3.5)
 atoms.set_calculator(EMT())
 calc = atoms.calc
 workdir = str(Path("Ni_ex").absolute())
@@ -31,7 +30,7 @@ fw_settings = {
     "fw_name": "phonons",
     "fw_spec": None,
     "out_spec": "relaxed_atoms",
-    "mod_spec_add": "calculated_forces",
+    "mod_spec_add": "ph_forces",
     "spec": {}
 }
 
@@ -41,12 +40,12 @@ kwargs_init = {"supercell_matrix": smatrix, "displacement": 0.01}
 kwargs_init_fw_out = {"workdir": workdir}
 
 init_fw = generate_firework(
-    func="hilde.phonopy.workflow.preprocess",
+    func="hilde.tasks.fireworks.preprocess_ph_ph3",
     func_fw_out="hilde.tasks.fireworks.fw_action_outs.add_phonon_force_calcs",
-    func_kwargs=kwargs_init,
+    func_kwargs={"phonopy_settings": kwargs_init},
     atoms=atoms,
     calc=calc,
-    func_fw_out_kwargs=kwargs_init_fw_out,
+    func_fw_out_kwargs={"phonopy_settings": kwargs_init_fw_out},
     atoms_calc_from_spec=False,
     fw_settings=fw_settings,
 )
@@ -61,7 +60,7 @@ anal_fw = generate_firework(
     func="hilde.phonopy.postprocess.postprocess",
     func_fw_out="hilde.tasks.fireworks.fw_action_outs.fireworks_no_mods_gen_function",
     args=[],
-    inputs=["metadata", fw_settings["mod_spec_add"]],
+    inputs=["metadata_ph", fw_settings["mod_spec_add"]],
     func_kwargs=kwargs,
     fw_settings=fw_settings,
 )
