f7680044d965021fd473033df9b4c77e6d452d8f
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-01-22 16:53:09 +0100

    hilde | include __version__ attribute

--- hilde/__init__.py
+++ hilde/__init__.py
@@ -1,12 +1,15 @@
 """ useful things to import """
 
+import pkg_resources
+
+__version__ = str(pkg_resources.require("hilde")[0].version)
 
 from ._defaults import (
     DEFAULT_CONFIG_FILE,
     DEFAULT_FIREWORKS_FILE,
     DEFAULT_GEOMETRY_FILE,
     DEFAULT_SETTINGS_FILE,
-    supported_tasks
+    supported_tasks,
 )
 from .settings import Settings, Configuration
 from .templates.aims import setup_aims
