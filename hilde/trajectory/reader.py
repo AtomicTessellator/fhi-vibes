c95dd20e8c24e5af1e0a2a2b0a0aea9ab18ab097
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-01-24 16:36:00 +0100

    revert to old system

--- hilde/trajectory/reader.py
+++ hilde/trajectory/reader.py
@@ -46,18 +46,5 @@ def reader(file, get_metadata=False):
 
         trajectory.append(atoms)
     if get_metadata:
-        return trajectory, CaseInsensitiveDict(metadata)
+        return trajectory, metadata
     return trajectory
-
-class CaseInsensitiveDict(dict):
-    def __init__(self, d):
-        self._d = d
-        self._s = dict((k.lower(), k) for k in d)
-    def __contains__(self, k):
-        return k.lower() in self._s
-    def __len__(self):
-        return len(self._s)
-    def __iter__(self):
-        return iter(self._s)
-    def __getitem__(self, k):
-        return self._d[self._s[k.lower()]]
