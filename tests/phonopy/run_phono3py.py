9506067d3dca19f6e36ad237da8bfcce264a0c08
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-01-28 13:21:09 +0100

    add phono3py workflow test

---
+++ tests/phonopy/run_phono3py.py
@@ -0,0 +1,11 @@
+""" use the hilde phonopy workflow """
+
+from ase.build import bulk
+from ase.calculators.emt import EMT
+from hilde import run_phono3py
+
+atoms = bulk("Al")
+
+calc = EMT()
+
+run_phono3py(atoms=atoms, calculator=calc)
