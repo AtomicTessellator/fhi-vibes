86de81197a5fe614ee96c6b550c5140730cb0bab
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-01-28 13:05:49 +0100

    phono3py example with pre-computed phonopy trajectory

--- examples/phono3py/run_phono3py.py
+++ examples/phono3py/run_phono3py.py
@@ -1,7 +1,6 @@
 """ Use the hilde.phonopy workflow to i) run the necessary force calculations
     and ii) create a phonopy object including force constants etc. """
 
-from hilde import run_phonopy, run_phono3py
+from hilde import run_phono3py
 
-run_phonopy()
 run_phono3py()
