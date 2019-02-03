2c3efcbeeeb831f892ef1eee2ca3a8cda5d6f771
Flo-Think <florian_knoop@gmx.de>
2019-02-03 21:47:52 +0100

    phonopy postprocess | write the supercell and force constants (again)

--- hilde/phonopy/postprocess.py
+++ hilde/phonopy/postprocess.py
@@ -2,9 +2,11 @@
 from pathlib import Path
 
 from hilde.helpers.converters import dict2results
-from hilde.phonopy.wrapper import prepare_phonopy
+from hilde.phonopy.wrapper import prepare_phonopy, get_force_constants
 from hilde.trajectory import reader
 from hilde.helpers.pickle import psave
+from hilde.io import write
+
 
 def postprocess(
     trajectory="phonopy/trajectory.yaml", pickle_file="phonon.pick", **kwargs
@@ -15,7 +17,9 @@ def postprocess(
     calculated_atoms, metadata = reader(trajectory, True)
 
     primitive = dict2results(metadata["Phonopy"]["primitive"])
+    supercell = dict2results(metadata["atoms"])
     supercell_matrix = metadata["Phonopy"]["supercell_matrix"]
+    supercell.info = {"supercell_matrix": str(supercell_matrix.flatten())}
     symprec = metadata["Phonopy"]["symprec"]
 
     phonon = prepare_phonopy(primitive, supercell_matrix, symprec=symprec)
@@ -26,5 +30,15 @@ def postprocess(
     phonon.produce_force_constants(force_sets)
     if pickle_file:
         psave(phonon, trajectory.parent / pickle_file)
+
+    # Save the supercell
+    fname = "geometry.in.supercell"
+    write(supercell, fname)
+    print(f"Supercell written to {fname}")
+
+    force_constants = get_force_constants(phonon)
+    fname = "force_constants.dat"
+    np.savetxt(fname, force_costants)
+    print(f"Force constants saved to {fname}.")
 
     return phonon
