c6e56b1ec2486254c7f05c233316a288d8c27e57
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-04-17 12:30:05 +0200

    tasks | add `run()` function that launches workflow

--- examples/phonopy/run_phonopy.py
+++ examples/phonopy/run_phonopy.py
@@ -1,6 +1,6 @@
 """ Use the hilde.phonopy workflow to i) run the necessary force calculations
     and ii) create a phonopy object including force constants etc. """
 
-from hilde.phonopy import run_phonopy
+from hilde import run
 
-run_phonopy()
+run()
