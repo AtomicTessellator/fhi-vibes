74733cf55779b7109a28196178a053922d2fc3c5
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-05-02 14:48:40 +0200

    reintroduced two naked excepts since they are needed for phono3py to not be a default requirement

--- hilde/phonon_db/database_interface.py
+++ hilde/phonon_db/database_interface.py
@@ -128,9 +128,8 @@ def to_database(
                 key_val_pairs["pair_distance_cutoff"] = phonon._displacement_dataset[
                     "cutoff_distance"
                 ]
-        except ModuleNotFoundError:
+        except:
             raise IOError("phonon3py is not installed, either phonon is of a type that is not defined or phono3py object")
-        raise IOError("phonon is of a type that is not defined")
     atoms.set_calculator(calc)
     if "calc_hash" in key_val_pairs or "atoms_hash" in key_val_pairs:
         warn("Replacing the atoms and calc hashes")
@@ -214,7 +213,7 @@ def obj2dict(obj):
 
             if isinstance(obj, Phono3py):
                 return phonon3_to_dict(obj)
-        except ModuleNotFoundError:
+        except:
             raise IOError("Phono3py is not installed, obj has to be a dict, Phonopy, or Atoms object")
         raise IOError("obj has to be a dict, Phonopy, Phono3py, or Atoms object")
 
