98a792a1aa303c4ed9a4292b3eb00332a7381123
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-05-29 11:27:39 +0200

    hash | use json string that would be written to trajectory for hashing
    - makes sure the same rounding is used for floats
    - reduce number for significant digits from 17 to 14 to remove ambiguitiy

--- hilde/helpers/hash.py
+++ hilde/helpers/hash.py
@@ -4,13 +4,29 @@ from json import dumps
 from pathlib import Path
 from configparser import ConfigParser
 from hashlib import sha1 as hash_sha
-from .converters import atoms2json, get_json
+from .converters import atoms2dict, atoms2json, get_json
+from .fileformats import dict2json
 
-def hashfunc(string, empty_str=""):
+
+def hashfunc(string, empty_str="", digest=True):
     """ Wrap the sha hash function and check for empty objects """
     if string in ("", "[]", "{}", "None"):
         string = empty_str
+    if digest:
+        return hash_sha(string.encode("utf8")).hexdigest()
     return hash_sha(string.encode("utf8"))
+
+
+def hash_atoms(atoms):
+    """hash the atoms object as it would be written to trajectory"""
+    a = atoms.copy()
+    a.info = {}
+
+    rep = dict2json(atoms2dict(a))
+
+    atoms_hash = hashfunc(rep)
+
+    return atoms_hash
 
 
 def hash_atoms_and_calc(
@@ -38,16 +54,18 @@ def hash_atoms_and_calc(
     )
 
     atomshash = hashfunc(atomsjson).hexdigest()
-    calchash = hashfunc(calcjson).hexdigest()
+    calchash = hashfunc(calcjson)
 
     return atomshash, calchash
+
 
 def hash_traj(ca, meta, hash_meta=False):
     ca_dct = [atoms2json(at) for at in ca]
     dct = dict(meta, calculated_atoms=ca_dct)
     if hash_meta:
-        return hashfunc(dumps(dct)).hexdigest(), hashfunc(dumps(meta)).hexdigest()
-    return hashfunc(dumps(dct)).hexdigest()
+        return hashfunc(dumps(dct)), hashfunc(dumps(meta))
+    return hashfunc(dumps(dct))
+
 
 def hash_dict(dct):
     if "calculator_parameters" in dct:
@@ -56,15 +74,4 @@ def hash_dict(dct):
                 dct["calculator_parameters"]["species_dir"]
             ).parts[-1]
 
-    return hashfunc(get_json(dct)).hexdigest()
-
-def hash_atoms(atoms):
-    """ hash only the atoms object """
-    hash_atoms = atoms.copy()
-    hash_atoms.info = {}
-
-    atoms_json, _ = atoms2json(atoms)
-
-    atoms_hash = hashfunc(atoms_json).hexdigest()
-
-    return atoms_hash
+    return hashfunc(get_json(dct))
