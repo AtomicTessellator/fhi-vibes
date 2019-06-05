0d5943714e3324691cefc9bb590d8790975c88af
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-06-05 22:11:37 +0200

    hilde.cfg | rename to .hilderc which lives in the home directory

--- hilde/_defaults.py
+++ hilde/_defaults.py
@@ -2,10 +2,12 @@
 
 from pathlib import Path
 
-supported_tasks = ["relaxation", "phonopy", "phono3py", "md"]
+supported_tasks = ["phonopy", "phono3py", "md"]
 
-DEFAULT_CONFIG_FILE = Path(__file__).parents[1] / "hilde.cfg"
-DEFAULT_FIREWORKS_FILE = Path(__file__).parents[1] / "fireworks.cfg"
+HOME = Path().home()
+
+DEFAULT_CONFIG_FILE = HOME / '.hilderc'
+DEFAULT_FIREWORKS_FILE = HOME / ".fireworksrc"
 DEFAULT_GEOMETRY_FILE = "geometry.in"
 DEFAULT_SETTINGS_FILE = "settings.in"
 DEFAULT_TEMP_SETTINGS_FILE = "temp_settings.in"
