c27f383b18cb2618b8851ff0102dc52a6922cd23
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-06-27 13:39:21 +0200

    I missed the task_generator file for the docstring updates

--- hilde/fireworks/workflows/task_generator.py
+++ hilde/fireworks/workflows/task_generator.py
@@ -3,16 +3,28 @@ from fireworks import PyTask
 
 
 def setup_atoms_task(task_spec, atoms, calc, fw_settings):
-    """
-    Setups an ASE Atoms task
-    Parameters:
-        task_spec (TaskSpec): Specification of the Firetask
-        atoms (dict): Dictionary representation of the ase.atoms.Atoms
-        calc (dict): Dictionary representation of the ASE Calculator Object
-        fw_settings (dict): FireWorks specific parameters
+    """Setups an ASE Atoms task
+    Parameters
+    ----------
+    task_spec: TaskSpec
+        Specification of the Firetask
+    atoms: dict
+        Dictionary representation of the ase.atoms.Atoms
+    calc: dict
+        Dictionary representation of the ASE Calculator Object
+    fw_settings: dict
+        FireWorks specific parameters
 
-    Returns:
-        (tuple): PyTask function name, PyTask args, PyTask inputs, PyTask kwargs
+    Returns
+    -------
+    pt_func: str
+        PyTask function name
+    pt_args: list
+        PyTask args
+    pt_inputs: list of str
+         PyTask inputs
+    pt_kwargs: dict
+        PyTask kwargs
     """
     pt_func = "hilde.fireworks.tasks.general_py_task.atoms_calculate_task"
     pt_args = task_spec.pt_args[:4]
@@ -31,14 +43,24 @@ def setup_atoms_task(task_spec, atoms, calc, fw_settings):
 
 
 def setup_general_task(task_spec, fw_settings):
-    """
-    Setups a general task
-    Parameters:
-        task_spec (TaskSpec): Specification of the Firetask
-        fw_settings (dict): FireWorks specific parameters
+    """Setups a general task
+    Parameters
+    ----------
+    task_spec: TaskSpec
+        Specification of the Firetask
+    fw_settings: dict
+        FireWorks specific parameters
 
-    Returns:
-        (tuple): PyTask function name, PyTask args, PyTask inputs, PyTask kwargs
+    Returns
+    -------
+    pt_func: str
+        PyTask function name
+    pt_args: list
+        PyTask args
+    pt_inputs: list of str
+         PyTask inputs
+    pt_kwargs: dict
+        PyTask kwargs
     """
     pt_args = task_spec.get_pt_args()
     pt_func = "hilde.fireworks.tasks.general_py_task.general_function_task"
@@ -48,16 +70,22 @@ def setup_general_task(task_spec, fw_settings):
 
 
 def generate_task(task_spec, fw_settings, atoms, calc):
-    """
-    Generates a PyTask for a Firework
-    Parameters:
-        task_spec (TaskSpec): Specification of the Firetask
-        fw_settings (dict): FireWorks specific parameters
-        atoms (dict): Dictionary representation of the ase.atoms.Atoms
-        calc (dict): Dictionary representation of the ASE Calculator Object
+    """Generates a PyTask for a Firework
+    Parameters
+    ----------
+    task_spec: TaskSpec
+        Specification of the Firetask
+    fw_settings: dict
+        FireWorks specific parameters
+    atoms: dict
+        Dictionary representation of the ase.atoms.Atoms
+    calc: dict
+        Dictionary representation of the ASE Calculator Object
 
-    Returns:
-        (PyTask): Task for the given TaskSpec
+    Returns
+    -------
+    PyTask
+        Task for the given TaskSpec
     """
     if task_spec.task_with_atoms_obj:
         pt_params = setup_atoms_task(task_spec, atoms, calc, fw_settings)
@@ -75,14 +103,18 @@ def generate_task(task_spec, fw_settings, atoms, calc):
 
 
 def generate_update_calc_task(calc_spec, updated_settings):
-    """
-    Generate a calculator update task
-    Parameters:
-        calc_spec (str): Spec for the calculator in the Fireworks database
-        updated_settings (dict): What parameters to update
+    """Generate a calculator update task
+    Parameters
+    ----------
+    calc_spec: str
+        Spec for the calculator in the Fireworks database
+    updated_settings: dict
+        What parameters to update
 
-    Returns:
-        (PyTask): Task to update the calculator in the Fireworks database
+    Returns
+    -------
+    PyTask
+        Task to update the calculator in the Fireworks database
     """
     return PyTask(
         {
@@ -94,18 +126,22 @@ def generate_update_calc_task(calc_spec, updated_settings):
 
 
 def generate_mod_calc_task(at, cl, calc_spec, kpt_spec):
-    """
-    Generate a calculator modifier task
-    Parameters:
-        at (dict or str): Either an Atoms dictionary or a spec key to get the
-                          Atoms dictionary for the modified system
-        cl (dict or str): Either a Calculator dictionary or a spec key to get
-                          the Calculator dictionary for the modified system
-        calc_spec (str): Spec for the calculator in the Fireworks database
-        kpt_spec (str): Spec to update the k-point density of the system
+    """Generate a calculator modifier task
+    Parameters
+    ----------
+    at: dict or str
+        Either an Atoms dictionary or a spec key to get the Atoms dictionary for the modified system
+    cl: dict or str
+        Either a Calculator dictionary or a spec key to get the Calculator dictionary for the modified system
+    calc_spec: str
+        Spec for the calculator in the Fireworks database
+    kpt_spec: str
+        Spec to update the k-point density of the system
 
-    Returns:
-        (PyTask): Task to update the calculator in the Fireworks database
+    Returns
+    -------
+    PyTask
+        Task to update the calculator in the Fireworks database
     """
     args = ["k_grid_density", calc_spec]
     kwargs = {"spec_key": kpt_spec}
