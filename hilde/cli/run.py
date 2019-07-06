f7e6a7e285f6a7f02e85feab45143514bafbda56
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-07-06 19:21:54 +0200

    run phonopy | add `--dry` flag

--- hilde/cli/run.py
+++ hilde/cli/run.py
@@ -6,6 +6,7 @@ from hilde.aims.context import AimsContext
 from hilde.phonopy.context import PhonopyContext
 from hilde.molecular_dynamics.context import MDContext
 from hilde.settings import Settings
+from hilde.helpers import cwd
 
 from .misc import AliasedGroup, click, complete_filenames
 
@@ -19,7 +20,7 @@ def run():
 
 
 @run.command("aims")
-@click.option("--workdir", default="aims", help="working directory")
+@click.option("--workdir", help="working directory")
 @click.option("--settings", default="aims.in", show_default=True, type=paths)
 @click.pass_obj
 def aims_run(obj, workdir, settings):
@@ -34,14 +35,21 @@ def aims_run(obj, workdir, settings):
 @run.command("phonopy")
 @click.option("--workdir", help="work directory")
 @click.option("--settings", default="phonopy.in", show_default=True, type=paths)
+@click.option("--dry", is_flag=True, help="just prepare inputs in the workdir")
 @click.pass_obj
-def phonopy_run(obj, workdir, settings):
+def phonopy_run(obj, workdir, settings, dry):
     """run a phonopy calculation"""
     from hilde.phonopy.workflow import run_phonopy
 
     ctx = PhonopyContext(Settings(settings_file=settings), workdir=workdir)
 
-    run_phonopy(ctx=ctx)
+    if dry:
+        with cwd(ctx.workdir, mkdir=True):
+            ctx.ref_atoms.write(ctx.settings.geometry.file)
+            ctx.settings.obj["workdir"] = "."
+            ctx.settings.write()
+    else:
+        run_phonopy(ctx=ctx)
 
 
 @run.command("md")
