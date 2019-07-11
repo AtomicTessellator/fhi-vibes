c9489a5f1b4ec8e7eda106d3c07e5fb476702013
Flo-Think <florian_knoop@gmx.de>
2019-07-12 00:47:53 +0200

    sobol | add test

---
+++ tests/test_sobol.py
@@ -0,0 +1,20 @@
+import numpy as np
+from hilde.helpers.sobol import RandomState
+
+
+def test_sobol():
+    rng = RandomState(dimension=3, seed=4)
+
+    try:
+        rng.rand(1)
+        raise RuntimeError("this should not happen")
+    except ValueError:
+        pass
+
+    rand = rng.rand(1, 3)
+    norm = np.linalg.norm(rand - np.array([0.95140271, 0.63109021, 0.34202771]))
+
+    assert norm < 1e-8, norm
+
+if __name__ == "__main__":
+    test_sobol()
