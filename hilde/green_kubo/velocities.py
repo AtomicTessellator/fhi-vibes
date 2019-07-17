5da0e550073b6f0538452fef5ad1d68fa4c6e7f9
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-07-17 14:17:45 +0200

    GK.velocities | make use of `xarray.DataArrays` to ship around the velocities

--- hilde/green_kubo/velocities.py
+++ hilde/green_kubo/velocities.py
@@ -3,23 +3,31 @@ import numpy as np
 import scipy.signal as sl
 import xarray as xr
 
+from hilde.trajectory import reader, Trajectory
 from hilde.fourier import compute_sed, get_frequencies
-from hilde.helpers import Timer
+from hilde.helpers import Timer, talk
 
 
-def get_velocites(trajectory, verbose=True):
-    """extract velocties from TRAJECTORY  and return as xarray.DataSet
+def get_velocities(trajectory, verbose=True):
+    """extract velocties from TRAJECTORY  and return as xarray.DataArray
 
     Args:
-        trajectory: list of atoms objects
-
+        trajectory (Trajectory or Path): list of atoms objects or where to find them
     Returns:
         velocities (xarray.DataArray [N_t, N_a, 3])
     """
     timer = Timer("Get velocities from trajectory", verbose=verbose)
 
-    times = trajectory.times
+    if not isinstance(trajectory, Trajectory):
+        trajectory = reader(trajectory)
 
+    metadata = {
+        "time unit": "fs",
+        "timestep": trajectory.timestep,
+        "atoms": trajectory[0].get_chemical_symbols(),
+    }
+
+    times = trajectory.times
     velocities = np.array([a.get_velocities() for a in trajectory])
 
     df = xr.DataArray(
@@ -27,6 +35,7 @@ def get_velocites(trajectory, verbose=True):
         dims=["time", "atom", "coord"],
         coords={"time": times},
         name="velocities",
+        attrs=metadata,
     )
 
     timer()
@@ -34,17 +43,19 @@ def get_velocites(trajectory, verbose=True):
     return df
 
 
-def get_velocity_aurocorrelation(trajectory, verbose=True):
-    """compute velocity autocorrelation function from trajectory
+def get_velocity_aurocorrelation(velocities=None, trajectory=None, verbose=True):
+    """compute velocity autocorrelation function from xarray
 
     Args:
+        velocities (xarray.DataArray [N_t, N_a, 3]): the velocities
         trajectory: list of atoms objects
     Returns:
         velocity_autocorrelation (xarray.DataArray [N_t, N_a, 3])
     """
-    velocities = get_velocites(trajectory)
+    if velocities is None and trajectory is not None:
+        velocities = get_velocities(trajectory, verbose=verbose)
 
-    timer = Timer("Get velocity autocorrelation from trajectory", verbose=verbose)
+    timer = Timer("Get velocity autocorrelation", verbose=verbose)
 
     Nt = len(velocities.time)
 
@@ -68,20 +79,23 @@ def get_velocity_aurocorrelation(trajectory, verbose=True):
     return df_corr
 
 
-def get_vdos(trajectory, verbose=True):
+def get_vdos(velocities=None, trajectory=None, verbose=True):
     r"""compute vibrational DOS for trajectory
 
     vdos(w) = FT{\sum_i corr(v_i, v_i)(t)}(w)
 
     Args:
+        velocities (xarray.DataArray [N_t, N_a, 3]): the velocities
         trajectory: list of atoms objects
-
     Returns:
         vdos (xarray.DataArray [N_t, N_a, 3])
     """
-    v_corr = get_velocity_aurocorrelation(trajectory)
+    if velocities is None and trajectory is not None:
+        velocities = get_velocities(trajectory, verbose=verbose)
 
-    timer = Timer("Get VDOS from trajectory", verbose=verbose)
+    v_corr = get_velocity_aurocorrelation(velocities)
+
+    timer = Timer("Get VDOS", verbose=verbose)
 
     omegas = get_frequencies(times=v_corr.time, verbose=verbose)
 
@@ -98,3 +112,21 @@ def get_vdos(trajectory, verbose=True):
     timer()
 
     return df_vdos
+
+
+def simple_plot(series, file="vdos.pdf"):
+    """simple plot of VDOS for overview purpose
+
+    Args:
+        series (pandas.Series): Intensity vs. omega
+    """
+    # find peaks:
+    peaks, *_ = sl.find_peaks(series)
+    high_freq = series.index[peaks[-1]]
+    ax = series.plot()
+    ax.set_xlim([0, 1.2 * high_freq])
+    ax.set_xlabel("Omega [THz]")
+    fig = ax.get_figure()
+    fig.savefig(file, bbox_inches="tight")
+    talk(f".. highest peak found at   {high_freq:.2f} THz")
+    talk(f".. plot saved to {file}")
