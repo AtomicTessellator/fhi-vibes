f62d1514f4a4e874466deede7ecdd7fe7a2a0839
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-08-05 19:09:22 +0200

    hilde.run | use arguments for settings

--- hilde/cli/run.py
+++ hilde/cli/run.py
@@ -21,11 +21,11 @@ def run():
 
 
 @run.command("aims")
+@click.argument("settings", default="aims.in", type=paths)
 @click.option("--workdir", help="working directory")
-@click.option("--settings", default="aims.in", show_default=True, type=paths)
 @click.pass_obj
-def aims_run(obj, workdir, settings):
-    """run one or several aims calculations"""
+def aims_run(obj, settings, workdir):
+    """run one or several aims calculations from SETTINGS (default: aims.in)"""
     from hilde.aims.workflow import run_aims
 
     ctx = AimsContext(Settings(settings_file=settings), workdir=workdir)
@@ -34,32 +34,26 @@ def aims_run(obj, workdir, settings):
 
 
 @run.command("phonopy")
+@click.argument("settings", default="phonopy.in", type=paths)
 @click.option("--workdir", help="work directory")
-@click.option("--settings", default="phonopy.in", show_default=True, type=paths)
 @click.option("--dry", is_flag=True, help="just prepare inputs in the workdir")
 @click.pass_obj
-def phonopy_run(obj, workdir, settings, dry):
-    """run a phonopy calculation"""
+def phonopy_run(obj, settings, workdir, dry):
+    """run a phonopy calculation for SETTINGS (default: phonopy.in)"""
     from hilde.phonopy.workflow import run_phonopy
 
     ctx = PhonopyContext(Settings(settings_file=settings), workdir=workdir)
 
-    if dry:
-        with cwd(ctx.workdir, mkdir=True):
-            ctx.ref_atoms.write(ctx.settings.geometry.file)
-            ctx.settings.obj["workdir"] = "."
-            ctx.settings.write()
-    else:
-        run_phonopy(ctx=ctx)
+    run_phonopy(ctx=ctx, dry=dry)
 
 
 @run.command("md")
+@click.argument("settings", default="md.in", type=paths)
 @click.option("--workdir", help="working directory")
-@click.option("--settings", default="md.in", show_default=True, type=paths)
 @click.option("--timeout", default=None, type=int, hidden=True)
 @click.pass_obj
-def md_run(obj, workdir, settings, timeout):
-    """run an MD simulation"""
+def md_run(obj, settings, workdir, timeout):
+    """run an MD simulation from SETTINS (default: md.in)"""
     ctx = MDContext(Settings(settings_file=settings), workdir=workdir)
 
     if obj.verbose > 0:
