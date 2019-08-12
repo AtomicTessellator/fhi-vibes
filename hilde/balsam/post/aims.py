166bd2635d9391d797fe4182c729042adaacaefc
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-08-12 11:38:20 +0200

    Removing the Balsam workflows from HiLDe
    Because they do not use the tradtional HiLDe workflows, I feel they are better off seperated

--- hilde/balsam/post/aims.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Check the completion of an aims calculation"""
-
-import balsam.launcher.dag as dag
-from hilde.balsam.data_encoder import decode
-from hilde.balsam.post.aims_calc_process import postprocess_aims
-
-postprocess_aims(decode(dag.current_job.data))
