de8a152408a63de9fe9b9fd82b9d363fde162d52
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-08-12 16:55:26 +0200

    tests | make fireworks test `pytest` compatible and rework `.gitlab-ci.yml`

--- tests/fireworks/fireworks_test.py
+++ tests/fireworks/fireworks_test.py
@@ -17,60 +17,71 @@ from hilde.phonopy.postprocess import postprocess
 
 from fireworks import Workflow
 
-db_name = os.getcwd() + "/test.db"
-print(f"database: {db_name}")
 
-atoms = bulk("Ni", "fcc", a=3.5)
-atoms.set_calculator(EMT())
-calc = atoms.calc
-workdir = str(Path("Ni_ex").absolute())
+def test_fireworks():
 
-smatrix = np.array([[-2, 2, 2], [2, -2, 2], [2, 2, -2]])
-atoms_hash, calc_hash = hash_atoms_and_calc(atoms)
+    db_name = os.getcwd() + "/test.db"
+    print(f"database: {db_name}")
 
-fw_settings = {
-    "serial": True,
-    "fw_name": "phonons",
-    "fw_spec": None,
-    "out_spec": "relaxed_atoms",
-    "mod_spec_add": "ph_forces",
-    "spec": {},
-}
+    atoms = bulk("Ni", "fcc", a=3.5)
+    atoms.set_calculator(EMT())
+    calc = atoms.calc
+    workdir = str(Path("Ni_ex").absolute())
 
-# set up the LaunchPad and reset it
-kwargs_init = {"supercell_matrix": smatrix, "displacement": 0.03, "serial": True}
-kwargs_init_fw_out = {"workdir": workdir, "serial": True}
+    smatrix = np.array([[-2, 2, 2], [2, -2, 2], [2, 2, -2]])
+    atoms_hash, calc_hash = hash_atoms_and_calc(atoms)
 
-init_fw = generate_firework(
-    func="hilde.fireworks.tasks.phonopy_phono3py_functions.bootstrap_phonon",
-    func_fw_out="hilde.fireworks.tasks.fw_out.phonons.post_init_mult_calcs",
-    func_kwargs={"ph_settings": kwargs_init},
-    atoms=atoms,
-    calc=calc,
-    args=[1.0],
-    func_fw_out_kwargs={"ph_settings": kwargs_init_fw_out},
-    atoms_calc_from_spec=False,
-    fw_settings=fw_settings,
-)
+    fw_settings = {
+        "serial": True,
+        "fw_name": "phonons",
+        "fw_spec": None,
+        "out_spec": "relaxed_atoms",
+        "mod_spec_add": "ph_forces",
+        "spec": {},
+    }
 
-kwargs = {"fireworks": True, "workdir": workdir + "/analysis"}
-task_spec_list = gen_phonon_analysis_task_spec(
-    "hilde.phonopy.postprocess.postprocess", kwargs, "ph_metadata", "ph_forces", "ph_times"
-)
+    # set up the LaunchPad and reset it
+    kwargs_init = {"supercell_matrix": smatrix, "displacement": 0.03, "serial": True}
+    kwargs_init_fw_out = {"workdir": workdir, "serial": True}
 
-anal_fw = generate_firework(task_spec_list, fw_settings=fw_settings)
+    init_fw = generate_firework(
+        func="hilde.fireworks.tasks.phonopy_phono3py_functions.bootstrap_phonon",
+        func_fw_out="hilde.fireworks.tasks.fw_out.phonons.post_init_mult_calcs",
+        func_kwargs={"ph_settings": kwargs_init},
+        atoms=atoms,
+        calc=calc,
+        args=[1.0],
+        func_fw_out_kwargs={"ph_settings": kwargs_init_fw_out},
+        atoms_calc_from_spec=False,
+        fw_settings=fw_settings,
+    )
 
-lp = LaunchPad(strm_lvl="INFO")
-lp.reset("", require_password=False)
-wf = Workflow([init_fw, anal_fw], {init_fw: [anal_fw]})
-lp.add_wf(wf)
+    kwargs = {"fireworks": True, "workdir": workdir + "/analysis"}
+    task_spec_list = gen_phonon_analysis_task_spec(
+        "hilde.phonopy.postprocess.postprocess",
+        kwargs,
+        "ph_metadata",
+        "ph_forces",
+        "ph_times",
+    )
 
-with cwd(workdir + "/fireworks", mkdir=True):
-    rapidfire(lp, wflow_id=wf.root_fw_ids, strm_lvl="INFO")
+    anal_fw = generate_firework(task_spec_list, fw_settings=fw_settings)
 
-phonon = postprocess(f"{workdir}/analysis/trajectory.son")
+    lp = LaunchPad(strm_lvl="INFO")
+    lp.reset("", require_password=False)
+    wf = Workflow([init_fw, anal_fw], {init_fw: [anal_fw]})
+    lp.add_wf(wf)
 
-phonon.set_mesh(3 * [5])
-_, _, frequencies, _ = phonon.get_mesh()
-print(f"Highest frequency: {frequencies.max():.3f} THz (Target: [8,10] THz)")
-assert 8 < frequencies.max() < 10
+    with cwd(workdir + "/fireworks", mkdir=True):
+        rapidfire(lp, wflow_id=wf.root_fw_ids, strm_lvl="INFO")
+
+    phonon = postprocess(f"{workdir}/analysis/trajectory.son")
+
+    phonon.set_mesh(3 * [5])
+    _, _, frequencies, _ = phonon.get_mesh()
+    print(f"Highest frequency: {frequencies.max():.3f} THz (Target: [8,10] THz)")
+    assert 8 < frequencies.max() < 10
+
+
+if __name__ == "__main__":
+    test_fireworks()
