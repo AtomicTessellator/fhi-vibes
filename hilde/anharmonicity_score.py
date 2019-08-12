ad4a5058c991c77f6094f52f7cefae32428c61dc
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-08-12 19:03:53 +0200

    r2 | add pearson coeffiecient, rework docstrings

--- hilde/anharmonicity_score.py
+++ hilde/anharmonicity_score.py
@@ -6,23 +6,46 @@ from hilde.helpers.displacements import get_dR
 from hilde.spglib.wrapper import get_symmetry_dataset
 
 
+def get_r(in_f_data, in_f_model):
+    r"""Calculate Pearson coefficient between f_data and f_model
+
+    Refrence Website
+    https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
+
+    Args:
+        in_f_data: input data
+        in_f_model: input model data
+
+    Returns:
+        float: Pearson coefficient
+    """
+
+    f_data = np.ravel(in_f_data)
+    f_model = np.ravel(in_f_model)
+
+    assert f_data.shape == f_model.shape, "Check shape of input arrays!"
+
+    f_data_mean = np.mean(f_data, axis=0)
+    f_model_mean = np.mean(f_model, axis=0)
+    cov = (f_data - f_data_mean) @ (f_model - f_model_mean)
+    sigma1 = np.sqrt((f_data - f_data_mean) @ (f_data - f_data_mean))
+    sigma2 = np.sqrt((f_model - f_model_mean) @ (f_model - f_model_mean))
+
+    return cov / sigma1 / sigma2
+
+
 def get_r2(in_f_data, in_f_model):
     r"""Calculate coefficient of determination between f_data and f_model
 
     Refrence Website
     https://en.wikipedia.org/wiki/Coefficient_of_determination#Definitions
 
-    Parameters
-    -----------
-    in_f_data: array
-        input data
-    in_f_model: array
-        input model data
+    Args:
+        in_f_data: input data
+        in_f_model: input model data
 
-    Returns
-    -------
-    r2: float
-        Coefficient of Determination
+    Returns:
+        float: Coefficient of Determination
     """
 
     f_data = np.ravel(in_f_data)
@@ -42,23 +65,15 @@ def get_r2_per_atom(
 ):
     """Compute r^2 score per atom in primitive cell. Optionally use symmetry.
 
-    Parameters
-    ----------
-    forces_dft: list
-        forces from dft calculations
-    forces_harmonic: list
-        forces from harmonic approximation
-    ref_structure: ase.atoms.Atoms
-        reference structure for symmetry analysis
-    reduce_by_symmetry: bool
-        project on symmetry equivalent instead of primitive
+    Args:
+        forces_dft: forces from dft calculations
+        forces_harmonic: forces from harmonic approximation
+        ref_structure: reference structure for symmetry analysis
+        reduce_by_symmetry: project on symmetry equivalent instead of primitive
 
-    Returns
-    -------
-    unique_atoms: list
-        the atoms from ref_structure for which r^2 was computed
-    r2_per_atom: list
-        r^2 score for atoms in unique_atoms
+    Returns:
+        unique_atoms: the atoms from ref_structure for which r^2 was computed
+        r2_per_atom: r^2 score for atoms in unique_atoms
     """
 
     sds = get_symmetry_dataset(ref_structure)
@@ -68,12 +83,15 @@ def get_r2_per_atom(
     else:
         compare_to = sds.mapping_to_primitive
 
+    if np.shape(forces_dft)[1] == 3 * len(ref_structure):
+        compare_to = compare_to.repeat(3)
+
     unique_atoms = np.unique(compare_to)
 
     r2_atom = []
     for u in unique_atoms:
         # which atoms belong to the prototype?
-        mask = compare_to.repeat(3) == u
+        mask = compare_to == u
         # take the forces that belong to this atom
         f_dft = forces_dft[:, mask]
         f_ha = forces_harmonic[:, mask]
@@ -84,23 +102,16 @@ def get_r2_per_atom(
 
 
 def get_forces_from_trajectory(trajectory, ref_structure=None, force_constants=None):
-    """get forces from trajectory
+    """get forces from trajectory, consider to use `trajectory.set_harmonic_forces`
 
-    Parameters
-    ----------
-    trajectory: list
-        list of Atoms objects with forces
-    ref_structure: ase.atoms.Atoms
-        reference Atoms object
-    force_constants: np.ndarray
-        force constants in [3N, 3N] shape
+    Args:
+        trajectory: list of Atoms objects with forces
+        ref_structure: reference Atoms object
+        force_constants: force constants in [3N, 3N] shape
 
-    Returns
-    -------
-    forces_dft: np.ndarray
-        DFT forces in [N_steps, 3N] shape
-    forces_harmonic: np.ndarray
-        harmonic forces in [N_steps, 3N] shape
+    Returns:
+        forces_dft: DFT forces in [N_steps, 3N] shape
+        forces_harmonic: harmonic forces in [N_steps, 3N] shape
     """
 
     f_ha = get_harmonic_forces
@@ -114,18 +125,12 @@ def get_forces_from_trajectory(trajectory, ref_structure=None, force_constants=N
 def get_harmonic_forces(sc, ref_structure, force_constants):
     """helper function: compute forces from force_constants
 
-    Parameters
-    ----------
-    sc: ase.atoms.Atoms
-        The distorted supercell
-    ref_structure: ase.atoms.Atoms
-        The undistorted structure
-    force_constants: np.ndarray
-        The force constant matrix
+    Args:
+        sc: The distorted supercell
+        ref_structure: The undistorted structure
+        force_constants: The force constant matrix
 
-    Results
-    -------
-    np.ndarray
+    Returns:
         The harmonic forces
     """
     return -force_constants @ get_dR(sc, ref_structure).flatten()
