437ce4ec52eb69633dbd684b2a895845a6e65ac3
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-09-11 11:45:26 +0200

    helpers.lists | add `reduce` and `expand` for lists with duplicate entries

--- hilde/helpers/lists.py
+++ hilde/helpers/lists.py
@@ -1,4 +1,20 @@
-"""helpers for lists"""
+"""helpers for lists and tuples"""
+from itertools import groupby
+
+
+def reduce_list(obj):
+    """reduce a with duplicate entries and return tuples of (count, entry)"""
+    return tuple((len(list(g)), k) for k, g in groupby(obj))
+
+
+def expand_list(obj):
+    """expand a list of tuples (count, entry) as produced ty `reduce_list`"""
+    if isinstance(obj[0], type(obj)):
+        lis = []
+        for l in (l * [g] for (l, g) in obj):
+            lis.extend(l)
+        return lis
+    return obj
 
 
 def list_dim(a):
