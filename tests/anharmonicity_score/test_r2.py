883b5e492489d5f0d50f89d83b18d2da06711a65
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-09-19 18:58:57 +0200

    tests | update anharmonicity score

--- tests/anharmonicity_score/test_r2.py
+++ tests/anharmonicity_score/test_r2.py
@@ -15,16 +15,15 @@ def test_r2():
     fc = parse_tdep_remapped_forceconstant(parent / "outfile.forceconstant_remapped")
 
     trajectory = reader(parent / "trajectory.son")
+    trajectory.force_constants = fc
 
     supercell = trajectory.supercell
 
-    f_dft, f_ha = score.get_forces_from_trajectory(trajectory, supercell, fc)
+    f_dft, f_ha = trajectory.forces, trajectory.forces_harmonic
 
     r2 = score.get_r2(f_dft, f_ha)
 
-    r2_per_atom = score.get_r2_per_atom(
-        f_dft, f_ha, supercell, reduce_by_symmetry=True
-    )
+    r2_per_atom = score.get_r2_per_atom(f_dft, f_ha, supercell, reduce_by_symmetry=True)
 
     ref_r2 = 0.56517063272
     ref_r2_per_atom = [0.316842934892, 0.544809575396, 0.580653088592]
