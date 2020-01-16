50f7c973114e2ec25ee9c70411f823354c66f7f2
Florian Knoop <knoop@fhi-berlin.mpg.de>
2020-01-16 11:47:57 +0100

    anharmonicity score | refactor and kick out some obsolete r2  routines
    * remember: r2 = 1 - sigmaA^2

--- vibes/anharmonicity_score.py
+++ vibes/anharmonicity_score.py
@@ -17,22 +17,23 @@ def _check_shape(f1, f2):
     )
 
 
-def get_sigma(f_data, f_model, assert_vanishing_mean=True, rtol=1e-5, axis=None):
+def get_sigma(f_data, f_model, rtol=1e-5, axis=None, silent=False):
     """Calculate RMSE / STD
 
     Args:
         f_data (np.ndarray): input data
         f_model (np.ndarray): input model data
-        assert_vanishing_mean (bool): make sure that the means vanish
         rtol (float): assert f.mean() / f.std() < rtol
         axis (tuple): axis along which mean and std are taken
+        silent (bool): silence warnings
     """
     f1 = np.asarray(f_data)
     f2 = np.asarray(f_model)
     _check_shape(f1, f2)
 
-    if assert_vanishing_mean:
-        assert np.all(f1.mean(axis=axis) < rtol * f1.std(axis=axis)), f1.mean(axis=axis)
+    # check f_data_mean, should be small
+    if np.any(f1.mean(axis=axis) > rtol * f1.std(axis=axis)) and not silent:
+        warn(f"f_data.mean(axis=axis) is {f1.mean(axis=axis)}", level=1)
 
     rmse = (f1 - f2).std(axis=axis)
     std = f1.std(axis=axis)
@@ -77,29 +78,20 @@ def get_r2(in_f_data, in_f_model, mean=True, silent=False):
     return 1 - Sres / Stot
 
 
-def get_r2_per_sample(f_data, f_model, skip=0):
-    """compute r2 for each sample"""
-    data = []
-    for fd, fm in zip(f_data[skip:], f_model[skip:]):
-        data.append(get_r2(fd, fm))
-    return np.array(data)
-
-
-def get_r2_per_atom(
-    forces_dft, forces_harmonic, ref_structure, reduce_by_symmetry=False, silent=True
+def get_sigma_per_atom(
+    forces_dft, forces_harmonic, ref_structure, reduce_by_symmetry=False
 ):
-    """Compute r^2 score per atom in primitive cell. Optionally use symmetry.
+    """Compute sigma score per atom in primitive cell. Optionally use symmetry.
 
     Args:
         forces_dft: forces from dft calculations in shape [Nt, Na, 3]
         forces_harmonic: forces from harmonic approximation in shape [Nt, Na, 3]
         ref_structure: reference structure for symmetry analysis
         reduce_by_symmetry: project on symmetry equivalent instead of primitive
-        silent (bool): silence the `get_r2` call
 
     Returns:
         unique_atoms: the atoms from ref_structure for which r^2 was computed
-        r2_per_atom: r^2 score for atoms in unique_atoms
+        sigma_per_atom: sigma score for atoms in unique_atoms
     """
 
     sds = get_symmetry_dataset(ref_structure)
@@ -120,7 +112,7 @@ def get_r2_per_atom(
     symbols = np.array(ref_structure.get_chemical_symbols())
     unique_atoms, counts = np.unique(compare_to, return_counts=True)
 
-    r2_atom = []
+    sigma_atom = []
     f_norms = []
     unique_symbols = []
     for u in unique_atoms:
@@ -131,11 +123,11 @@ def get_r2_per_atom(
         f_dft = forces_dft[:, mask]
         f_ha = forces_harmonic[:, mask]
         # compute r^2
-        r2_atom.append(get_r2(f_dft, f_ha, silent=silent))
+        sigma_atom.append(get_sigma(f_dft, f_ha))
         f_norms.append(float(f_dft.std()))
 
     # compute weighted mean
-    mean = (np.array(r2_atom) @ counts) / counts.sum()
+    mean = (np.array(sigma_atom) @ counts) / counts.sum()
 
     attrs = {
         "unique_atoms": unique_atoms,
@@ -145,25 +137,51 @@ def get_r2_per_atom(
         "mean": mean,
     }
 
-    ret = xr.DataArray(np.array(r2_atom), attrs=attrs)
+    ret = xr.DataArray(np.array(sigma_atom), attrs=attrs)
 
     return ret
 
 
-def get_r2_per_direction(f_data, f_model):
-    """compute r2 for each Cartesian direction"""
+def get_sigma_per_direction(f_data, f_model):
+    """compute sigma for each Cartesian direction"""
     assert f_data.shape[-1] == 3
     directions = ("x", "y", "z")
     y = np.asarray(f_data).swapaxes(-1, 0)
     f = np.asarray(f_model).swapaxes(-1, 0)
 
-    r2_direction = [get_r2(y[xx], f[xx]) for xx in range(len(directions))]
-    return r2_direction
+    sigma_direction = [get_sigma(y[xx], f[xx]) for xx in range(len(directions))]
+    return sigma_direction
+
+
+def get_sigma_dict(f, fh, ref_atoms, by_symmetry=False, per_direction=False):
+    """get sigma dictionary for set of forces `f` and harmonic forces `fh`"""
+    dct = {}
+    dct.update({"sigma": get_sigma(f, fh)})
+
+    sigmas = get_sigma_per_atom(f, fh, ref_atoms, reduce_by_symmetry=by_symmetry)
+    dct.update({"sigma_atom_mean": sigmas.attrs["mean"]})
+    d = {}
+    for sym, r in zip(sigmas.attrs["symbols"], sigmas):
+        k = _key(sym, d)
+        d[k] = float(r)
+
+    dct.update(d)
+
+    if per_direction:
+        directions = ("x", "y", "z")
+        sigmas = get_sigma_per_direction(f, fh)
+        d = {}
+        for ii, xx in enumerate(directions):
+            d[f"sigma [{xx}]"] = sigmas[ii]
+
+        dct.update(d)
+
+    return dct
 
 
 def get_dataframe(dataset, per_sample=False, per_direction=False, by_symmetry=False):
     """return anharmonicity dataframe for xarray.Dataset DS"""
-    from pandas import DataFrame
+    import pandas as pd
     from vibes.helpers.converters import json2atoms
 
     DS = dataset
@@ -176,62 +194,25 @@ def get_dataframe(dataset, per_sample=False, per_direction=False, by_symmetry=Fa
     name = DS.attrs["System Name"]
 
     dct = {}
+    key = f"{name}"
+    dct[key] = get_sigma_dict(fs, fhs, ref_atoms, by_symmetry, per_direction)
+
+    # add information per sample
     if per_sample:
         for ii, (f, fh) in enumerate(zip(progressbar(fs), fhs)):
-
             key = f"{name}.{ii:04d}"
-            dct[key] = {"r2 (RMS)": get_r2(f, fh)}
+            dct[key] = get_sigma_dict(f, fh, ref_atoms, by_symmetry, per_direction)
 
-            r2s = get_r2_per_atom(f, fh, ref_atoms, reduce_by_symmetry=by_symmetry)
-            dct[key].update({"r2_atom_mean": r2s.attrs["mean"]})
-            d = {}
-            for sym, r in zip(r2s.attrs["symbols"], r2s):  # enumerate(r2s):
-                k = _key(sym, d)
-                d[k] = float(r)
+    df = pd.DataFrame(dct)
 
-            dct[key].update(d)
-
-            if per_direction:
-                directions = ("x", "y", "z")
-                r2s = get_r2_per_direction(f, fh)
-                d = {}
-                for ii, xx in enumerate(directions):
-                    d[f"r2 [{xx}]"] = r2s[ii]
-
-                dct[key].update(d)
-
-    else:
-        key = f"{name}"
-        dct[key] = {"r2 (RMS)": get_r2(fs, fhs)}
-
-        r2s = get_r2_per_atom(fs, fhs, ref_atoms, reduce_by_symmetry=by_symmetry)
-        dct[key].update({"r2_atom_mean": r2s.attrs["mean"]})
-        d = {}
-        for sym, r in zip(r2s.attrs["symbols"], r2s):  # enumerate(r2s):
-            k = _key(sym, d)
-            d[k] = float(r)
-
-        dct[key].update(d)
-
-        if per_direction:
-            directions = ("x", "y", "z")
-            r2s = get_r2_per_direction(fs, fhs)
-            d = {}
-            for ii, xx in enumerate(directions):
-                d[f"r2 [{xx}]"] = r2s[ii]
-
-            dct[key].update(d)
-
-    df = DataFrame(dct)
-    # df[name] = df.mean(axis=1)
     return df.T
 
 
 def _key(sym, d):
-    key = f"r2 [{sym}]"
+    key = f"sigma [{sym}]"
     if key in d:
         for ii in range(1, 100):
-            key = f"r2 [{sym}{ii}]"
+            key = f"sigma [{sym}{ii}]"
             if key not in d:
                 break
     return key
