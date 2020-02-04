3c64502c895080e71a787f11f4edbd418410e46c
Florian Knoop <knoop@fhi-berlin.mpg.de>
2020-02-04 17:57:09 +0100

    gk | vdos add test

---
+++ tests/green_kubo/test_cli_vdos.py
@@ -0,0 +1,19 @@
+import subprocess as sp
+from pathlib import Path
+
+import pytest
+
+parent = Path(__file__).parent
+
+
+commands = ["vibes aigk vdos test.nc -p"]
+
+
+@pytest.mark.parametrize("cmd", commands)
+def test_cmd(cmd):
+    sp.run(cmd.split(), cwd=parent, check=True)
+
+
+if __name__ == "__main__":
+    for cmd in commands:
+        test_cmd(cmd)
