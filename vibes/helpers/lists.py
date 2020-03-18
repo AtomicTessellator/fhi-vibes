c7c1f386a0f4479854365a14cc76294f2546d81c
Florian Knoop <florian_knoop@gmx.de>
2020-03-18 13:02:33 +0100

    helpers | remove unused code and clean up some files

--- vibes/helpers/lists.py
+++ vibes/helpers/lists.py
@@ -20,35 +20,13 @@ def expand_list(obj):
     return obj
 
 
-def list_dim(a):
-    """dimension of a (nested) pure Python list
-
-    Parameters
-    ----------
-    a: list
-        The input list
-
-    Returns
-    -------
-    int
-        The dimension of the pure python list
-    """
+def list_dim(a: list) -> int:
+    """dimension of a (nested) pure Python list, similar to np.shape"""
     if not type(a) == list:
         return []
     return [len(a)] + list_dim(a[0])
 
 
-def list2str(lis):
-    """convert list to string
-
-    Parameters
-    ----------
-    lis: list
-        list to convert to str
-
-    Returns
-    -------
-    str
-        The json string version of the list
-    """
+def list2str(lis: list) -> str:
+    """convert list to string"""
     return "[{}]".format(", ".join([str(el) for el in lis]))
