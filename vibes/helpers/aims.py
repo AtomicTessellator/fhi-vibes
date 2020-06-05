a733012fb97c72255baa9a66177d1b50ade87d6c
Florian Knoop <florian_knoop@gmx.de>
2020-06-05 12:11:00 +0200

    aims_uuid | don't print message when `aims_uuid` was not found

--- vibes/helpers/aims.py
+++ vibes/helpers/aims.py
@@ -4,11 +4,12 @@ from vibes.filenames import filenames
 from vibes.helpers import talk, warn
 
 
-def peek_aims_uuid(file: str = filenames.output.aims) -> str:
+def peek_aims_uuid(file: str = filenames.output.aims, verbose: bool = False) -> str:
     """peek into aims.out and find the uuid
 
     Args:
         file: Path to the aims.out file (default: aims.out)
+        verbose: be verbose
 
     Returns:
         The uuid of an FHI-Aims calculation
@@ -19,7 +20,8 @@ def peek_aims_uuid(file: str = filenames.output.aims) -> str:
             line = next(l for l in f if "aims_uuid" in l)
             return line.split()[-1]
     except FileNotFoundError:
-        talk(f"No aims_uuid found.")
+        if verbose:
+            talk(f"No aims_uuid found.")
     except StopIteration:
         warn(f"{file} presumably empty, no aims_uuid found")
     return ""
