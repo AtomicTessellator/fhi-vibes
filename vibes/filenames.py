9051e632805ce0ea0678aab053bcfb63c536332e
Florian Knoop <florian_knoop@gmx.de>
2020-06-07 14:00:41 +0200

    filenames | add names of force constants files

--- vibes/filenames.py
+++ vibes/filenames.py
@@ -10,13 +10,27 @@ _filename = Path(__file__).stem
 _geometry = DEFAULT_GEOMETRY_FILE
 
 _suffixes = "suffixes"
-_dct = {"next_step": ".next_step"}
-suffixes = collections.namedtuple(_suffixes, _dct.keys())(**_dct)
+_dct = {
+    "next_step": ".next_step",
+    "tdep_fc": ".forceconstant",
+    "tdep_fc_remapped": ".forceconstant_remapped",
+}
+suffixes = collections.namedtuple("suffixes", _dct.keys())(**_dct)
 
 
+# output file names
 _output = "output"
 _dct = {"aims": "aims.out"}
 output = collections.namedtuple(_output, _dct.keys())(**_dct)
+
+# force constant names
+_fc = "fc"
+_dct = {
+    "phonopy": "FORCE_CONSTANTS",
+    "phonopy_hdf5": "fc2.hdf5",
+    "phonopy_remapped": "FORCE_CONSTANTS_remapped",
+}
+fc = collections.namedtuple(_fc, _dct.keys())(**_dct)
 
 
 _dct = {
@@ -28,5 +42,7 @@ _dct = {
     "trajectory_dataset": keys.trajectory + ".nc",
     "deformation": "deformation.dat",
     _output: output,
+    _fc: fc,
+    _suffixes: suffixes,
 }
 filenames = collections.namedtuple(_filename, _dct.keys())(**_dct)
