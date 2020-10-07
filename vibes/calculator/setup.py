9885e9e7544c3e18f0756bfa9840e6a49c367929
Marcel Langer <me@sirmarcel.com>
2020-10-07 15:19:37 +0200

    ENH: Support non-ase calculators
    With this tweak, `module` is added as
    parameter for the custom calculator section,
    so non-`ase` calculators can be used with `vibes`.

--- vibes/calculator/setup.py
+++ vibes/calculator/setup.py
@@ -1,5 +1,6 @@
 """set up ase calculators from settings"""
 from ase.calculators.calculator import Calculator, get_calculator_class
+from importlib import import_module
 
 from vibes import keys
 from vibes.helpers import talk
@@ -8,9 +9,14 @@ from vibes.helpers import talk
 def from_settings(settings: dict = None) -> Calculator:
     """get calculator class and create the calculator from settings.parameters"""
     calc_dict = settings[keys.calculator]
-    calc_name = calc_dict.name.lower()
+    calc_name = calc_dict.name
+    calc_module = calc_dict.get("module")
 
-    cls = get_calculator_class(calc_name)
+    if calc_module is None:  # ase calculator
+        cls = get_calculator_class(calc_name.lower())
+    else:
+        module = import_module(calc_module)
+        cls = getattr(module, calc_name)
 
     parameters = calc_dict.get(keys.parameters, {})
 
