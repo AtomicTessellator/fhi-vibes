853085f6e519d8a2c31ec37f4dd2ad9b6fb3dbd2
Florian Knoop <florian_knoop@gmx.de>
2021-03-11 14:02:50 +0100

    tests | add test that verifies change in 6798e3f5
    - in 6798e3f5, `reduce_fc` was fixed to correctly map back fro supercell to primitive indices
    - `test_reduce.py` checks that the roundtrip "phonopy fc" -> "remapped fc" -> "reduced fc" works

---
+++ tests/remap_fc/test_reduce.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import numpy as np
+from ase.io import read
+
+from vibes.helpers.force_constants import reduce_force_constants, remap_force_constants
+from vibes.helpers.supercell import map2prim
+from vibes.phonopy.utils import parse_phonopy_force_constants
+
+
+parent = Path(__file__).parent
+
+
+def test_reduce_fc():
+    fc_phonopy = parse_phonopy_force_constants(parent / "FORCE_CONSTANTS")
+
+    kw = {"format": "aims"}
+    primitive = read(parent / "geometry.in.primitive", **kw)
+    supercell = read(parent / "geometry.in.supercell", **kw)
+
+    kw = {"primitive": primitive, "supercell": supercell}
+    fc_remapped = remap_force_constants(fc_phonopy, **kw, symmetrize=False)
+
+    sc2pc = map2prim(**kw)
+    fc_reduced = reduce_force_constants(fc_remapped, sc2pc)
+    print(fc_reduced.shape)
+
+    diff = np.linalg.norm(fc_phonopy - fc_reduced)
+
+    print(diff)
+
+
+if __name__ == "__main__":
+    test_reduce_fc()
