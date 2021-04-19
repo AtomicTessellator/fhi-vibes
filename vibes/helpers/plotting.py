3d1f695135b540a9d2a9e2805a39acd8ea2f43fa
Florian Knoop <florian_knoop@gmx.de>
2021-04-19 18:05:28 +0200

    gk.info | add summary plotting functions
    - split out into dedicated scripts
    - add default rc params to `vibes.helpers.plotting`

--- vibes/helpers/plotting.py
+++ vibes/helpers/plotting.py
@@ -12,3 +12,29 @@ tableau_colors_int = [
 ]
 
 tableau_colors = [tuple(c / 255 for c in color) for color in tableau_colors_int]
+
+fontsize = 14
+
+rc_params = {
+    "font.family": "sans-serif",
+    "font.sans-serif": "DejaVu Sans",
+    "font.size": fontsize,
+    "xtick.direction": "in",
+    "ytick.direction": "in",
+    "savefig.dpi": 150,
+    "figure.titlesize": fontsize + 2,
+    "axes.facecolor": "white",  # axes background color
+    "axes.edgecolor": "1b1b1b",  # axes edge color
+    "axes.linewidth": 0.8,  # edge linewidth
+    "axes.titlesize": fontsize,
+    "xtick.major.size": 6,  # major tick size in points
+    "xtick.minor.size": 4,  # minor tick size in points
+    "xtick.major.width": 0.8,  # major tick width in points
+    "xtick.minor.width": 0.6,  # minor tick width in points
+    "xtick.color": "1b1b1b",  # color of the tick labels
+    "ytick.major.size": 6,  # major tick size in points
+    "ytick.minor.size": 4,  # minor tick size in points
+    "ytick.major.width": 0.8,  # major tick width in points
+    "ytick.minor.width": 0.6,  # minor tick width in points
+    "ytick.color": "1b1b1b",  # color of the tick labels
+}
