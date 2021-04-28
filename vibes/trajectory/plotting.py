bbaf4680c1e239a50b0a9feb94c0ee8ca48f0fa2
Florian Knoop <florian_knoop@gmx.de>
2021-04-28 22:03:42 +0200

    md.info | add max. displacements

--- vibes/trajectory/plotting.py
+++ vibes/trajectory/plotting.py
@@ -1,6 +1,5 @@
 """plot trajectory data"""
 from ase import units
-
 from vibes import keys
 
 
@@ -40,7 +39,7 @@ def plot_summary(dataframe, avg=50, natoms=None):
     avg_kw = {"linewidth": 1.5}
     fig_kw = {
         "figsize": (8.27, 11.69),
-        "gridspec_kw": {"height_ratios": [1, 1, 0.5, 0.5]},
+        # "gridspec_kw": {"height_ratios": [1, 1, 1, 1, 1]},
         "sharex": True,
     }
     kw_roll = {"window": avg, "min_periods": 0, "center": True}
@@ -52,7 +51,7 @@ def plot_summary(dataframe, avg=50, natoms=None):
     df_p_int = df_p.interpolate("akima")
     df_p = df_p.dropna()
     if len(df_p) > 3:
-        fig, (ax, ax2, ax3, ax4) = plt.subplots(nrows=4, **fig_kw)
+        fig, (ax, ax2, ax3, ax4, ax5) = plt.subplots(nrows=5, **fig_kw)
         # fig = plt.figure(**fig_kw)
         # gs = gridspec.GridSpec(nrows=6, ncols=1)
         # ax = fig.add_subplot(gs[0:2])
@@ -79,7 +78,7 @@ def plot_summary(dataframe, avg=50, natoms=None):
 
     else:
         fig_kw["gridspec_kw"] = {"height_ratios": [1, 1, 1]}
-        fig, (ax, ax2, ax4) = plt.subplots(nrows=3, **fig_kw)
+        fig, (ax, ax2, ax5) = plt.subplots(nrows=3, **fig_kw)
 
     temp.plot(color=tc[3], title="Nuclear Temperature", ax=ax, **plot_kw)
 
@@ -117,23 +116,41 @@ def plot_summary(dataframe, avg=50, natoms=None):
     ax2.legend(loc=4)
     ax2.set_ylabel("Energy [eV]")
 
+    # anharmonicity
+    if keys.sigma_per_sample in dataframe:
+        color = "C0"
+        sigmas = dataframe[keys.sigma_per_sample]
+        ax4.axhline(sigmas.mean(), c=color, lw=0.5)
+        sigmas.plot(ax=ax4, c=color, **plot_kw)
+        roll = sigmas.rolling(**kw_roll).mean()
+        roll.plot(ax=ax4, c=color)
+
+    ax4.set_title("Anharmonicity")
+    ax4.set_ylabel(r"$\sigma^{\rm A} (t)$")
+
     # displacements
+    # max
+    dr_max = dataframe.dr_max / 2
+    dr_max.plot(color=tc[6], ax=ax5, **plot_kw)
+    roll = dr_max.rolling(**kw_roll).mean()
+    roll.plot(color=tc[6], ax=ax5, label="max(|dR|)/2", **avg_kw)
+    # mean
     dr = dataframe.dr_mean
-    dr_std = dataframe.dr_std
-    dr.plot(color=tc[4], ax=ax4, **plot_kw)
+    dr.plot(color=tc[4], ax=ax5, **plot_kw)
     roll = dr.rolling(**kw_roll).mean()
-    roll.plot(color=tc[4], ax=ax4, label="mean(|dR|)", **avg_kw)
-
-    dr_std.plot(color=tc[5], ax=ax4, **plot_kw)
+    roll.plot(color=tc[4], ax=ax5, label="mean(|dR|)", **avg_kw)
+    # std
+    dr_std = dataframe.dr_std
+    dr_std.plot(color=tc[5], ax=ax5, **plot_kw)
     roll = dr_std.rolling(**kw_roll).mean()
-    roll.plot(color=tc[5], ax=ax4, label="std(|dR|)", **avg_kw)
+    roll.plot(color=tc[5], ax=ax5, label="std(|dR|)", **avg_kw)
     # ax4.fill_between(dr.index, dr + dr_std, dr - dr_std, color=tc[4], alpha=0.3)
-    ax4.set_ylabel("Displacement [Å]")
-    ax4.set_title("Displacement")
-    ax4.axhline(0)
-    ax4.legend(loc=4)
+    ax5.set_ylabel("Displacement [Å]")
+    ax5.set_title("Displacement")
+    ax5.axhline(0)
+    ax5.legend(loc=4)
 
-    ax4.set_xlabel("Time [ps]")
+    ax5.set_xlabel("Time [ps]")
 
     # fig.tight_layout()
     file = "md_summary.pdf"
