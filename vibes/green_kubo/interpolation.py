9548575fa1284bb66c9ee019e9709d9ea8cd12c2
Surefire618 <zhaosure168@gmail.com>
2023-02-07 18:04:32 +0100

    extrapolation update

--- vibes/green_kubo/interpolation.py
+++ vibes/green_kubo/interpolation.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.stats as st
 import xarray as xr
 from scipy.interpolate import LinearNDInterpolator, griddata
 from scipy.spatial.qhull import QhullError
@@ -106,6 +105,7 @@ def get_interpolation_data(
         }
     """
     from .harmonic import get_kappa
+    from scipy.optimize import curve_fit
 
     # define scaled lifetimes
     l_sq = dmx.w2_sq * np.nan_to_num(lifetimes)
@@ -152,7 +152,7 @@ def get_interpolation_data(
         ir_l_int_sq = interpolate_to_grid(q_points=grid.ir.points, **kw_train)
 
         # scale back to lifetimes
-        tau_int_sq = ir_l_int_sq[:, grid.ir.map2full] * solution.w_inv_sq ** 2
+        tau_int_sq = ir_l_int_sq[:, grid.ir.map2full] * solution.w_inv_sq**2
 
         # compute K on new grid, account for change in grid point number
         Nq_eff = len(grid.points) / Nq_init
@@ -167,12 +167,20 @@ def get_interpolation_data(
 
     # interpolate to infinitely dense grid assuming convergence with 1/nq (Riemann sum)
     ks = np.diagonal(Ks, axis1=1, axis2=2).mean(axis=1)
-    m, y0, *_, stderr = st.linregress(nqs ** -1.0, y=ks)
+    # m, y0, *_, stderr = st.linregress(nqs ** -1.0, y=ks)
+    # init linear fit parameters
+    p0 = -1, 10
+    popt, pcov = curve_fit(
+        lambda x, m, y0: m * x + y0, nqs**-1.0, ks, p0, sigma=nqs**-1.0
+    )
+    perr = np.sqrt(np.diag(pcov))
+    m, y0 = popt
+    stderr = perr[0]
 
     k_ha = np.diagonal(kappa_ha).mean()
     nq = len(dmx.q_points) ** (1 / 3)
 
-    correction = -m / nq
+    correction = y0 - k_ha  # -m / nq
     correction_factor = 1 + correction / k_ha
     correction_factor_err = stderr / nq / k_ha
 
