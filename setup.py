f9a0e802385ff0c740a233bead456c605b575249
Tom Purcell <purcellt@arizona.edu>
2024-03-29 10:42:20 -0700

    Modernize pyproject.toml
    Remove poetry as the strict poetry version is creating issues

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""provide numpy extension to compile fortran routines"""
+from numpy.distutils.core import setup, Extension  # noqa: F401
+
+setup(
+    ext_modules=[
+        Extension(
+            name="vibes.helpers.supercell.supercell",
+            sources=[
+                "vibes/helpers/supercell/linalg.f90",
+                "vibes/helpers/supercell/supercell.f90",
+            ],
+            extra_compile_args=["-O3"],
+        )        
+    ]
+)
+# ext = Extension(
+#     name="vibes.helpers.supercell.supercell",
+#     sources=[
+#         "vibes/helpers/supercell/linalg.f90",
+#         "vibes/helpers/supercell/supercell.f90",
+#     ],
+#     extra_compile_args=["-O3"],
+# )
+
+
+# def build(setup_kwargs):
+#     """add Extension to setup kwargs"""
+
+#     setup_kwargs.update({"ext_modules": [ext]})
