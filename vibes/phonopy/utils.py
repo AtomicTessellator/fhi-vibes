b86b57634c0be46daf79b8d19e71363f9dbdadc2
Surefire618 <zhaosure168@gmail.com>
2024-10-21 17:44:00 +0200

    Update atoms.get_reciprocal_cell() to atoms.cell.reciprocal(), the old one is deprecated since ase:3.21.0

--- vibes/phonopy/utils.py
+++ vibes/phonopy/utils.py
@@ -316,7 +316,7 @@ def remap_force_constants(
             pairs=sc_r,
             fc_in=force_constants,
             map2prim=map2prim,
-            inv_lattice=new_supercell.get_reciprocal_cell(),
+            inv_lattice=new_supercell.cell.reciprocal(),
             tol=tol,
             eps=eps,
         )
